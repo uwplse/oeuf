3c02dcaa8f17039bf468f7ab4e89edddbd4ffa09
Eric A Mullen <emullen@cs.washington.edu>
2016-04-22 19:23:37 -0700

    removing old compcert

--- compcert/gallina/Oeuf.v
+++
@@ -1,6 +0,0 @@
-Require Import Compiler.
-Require Import Gallina.
-Require Import Fib.
-
-Definition transf_gallina_to_asm := fun x => transf_clight_program (transf_program x).
-Definition test_prog := fib_prog.
--- compcert/test/regression/sizeof2.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-int main()
-{
-  printf("%d\n", sizeof("abcd"));
-  printf("%d\n", sizeof(L"abcd") / sizeof(wchar_t));
-  return 0;
-}
