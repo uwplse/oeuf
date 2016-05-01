65e4eb54cb37120f7b35621dbebac345f647cf5a
Eric A Mullen <emullen@cs.washington.edu>
2016-05-01 13:37:07 -0700

    Proved the reflection simulates

--- src/Oeuf.v
+++ src/Oeuf.v
@@ -1,5 +1,6 @@
 Require Import ZArith.
 Require Import compcert.lib.Integers.
+Require Import compcert.common.Smallstep.
 
 (* Object language types *)
 Module type.
@@ -39,6 +40,7 @@ Module expr.
     | If _ b e1 e2 => if denote b then denote e1 else denote e2
     end.
 
+  
 End expr.
 
 
@@ -70,11 +72,14 @@ Coercion Int.repr : Z >-> Int.int.
 Definition foo : int := if Int.eq (Int.add 3 7) 10 then 4 else 5.
 
 (* its reflection in our deep embedding *)
+(* make a function for now *)
 Definition reflect_foo :=
   expr.If (expr.IntEq (expr.IntAdd (expr.IntConst 3) (expr.IntConst 7))
                       (expr.IntConst 10))
-          (expr.IntConst 4)
-          (expr.IntConst 5).
+                   (expr.IntConst 4)
+                   (expr.IntConst 5).
+
+
 
 (* I wrote down the reflection for this program correctly. *)
 Example reflect_foo_reflects_foo :
@@ -87,3 +92,59 @@ Eval compute in compiler.compile reflect_foo.
 
 (* Now we want some sort of correctness theorem saying that the compiled expression
    evaluates to the same value as the original. *)
+
+Require Import compcert.cfrontend.Cstrategy.
+Require Import compcert.cfrontend.Csem.
+
+Module ID.
+
+  Definition id {A} (x : A) : A := x.
+  Hint Opaque id.
+End ID.
+
+
+
+(* the compilation steps to the reflection *)
+Definition reflection_sim (reflection : expr.syntax type.int) : Prop :=
+  forall ge fn c env m,
+    let orig := ExprState fn (ID.id (compiler.compile reflection)) c env m in
+    let res := ExprState fn (Csyntax.Eval (Values.Vint (ID.id (expr.denote reflection))) compiler.Tint) c env m in
+    star Cstrategy.estep ge orig Events.E0 res.
+
+
+(* compiled foo steps to denoted foo *)
+Lemma eval_foo :
+  reflection_sim reflect_foo.
+Proof.
+  unfold reflection_sim.
+  intros.
+  simpl.
+  econstructor.
+  (* first step *)
+  eapply step_condition.
+  econstructor; eauto.
+  repeat (econstructor; simpl; eauto).
+  simpl.
+  replace (Int.add 3 7) with (Int.repr 10) by ring.
+  rewrite Int.eq_true. simpl.
+  unfold Values.Vtrue. unfold Cop.bool_val.
+  simpl. reflexivity.
+  2: simpl; reflexivity.
+
+  (* second step *)
+  econstructor.
+
+  unfold Int.one. unfold Int.zero.
+  replace (Int.eq 1 0) with false. simpl.
+  replace (Int.add 3 7) with (Int.repr 10) by ring.
+  eapply step_paren; eauto. econstructor; eauto.
+  econstructor; eauto. simpl.
+  cbn. reflexivity.
+  unfold Int.eq. unfold Coqlib.zeq.
+  replace (Int.unsigned 1) with (1%Z) by auto.
+  replace (Int.unsigned 0) with (0%Z) by auto.
+  simpl. reflexivity.
+
+  (* no more steps *)
+  eapply star_refl. simpl. eauto.
+Qed.  
