0be03c79e1a6ed62c4027683ac186c637b9a79b9
Eric A Mullen <emullen@cs.washington.edu>
2016-05-18 12:04:46 -0700

    having trouble with mutual recursion wip

--- src/Emajortodmajor.v
+++ src/Emajortodmajor.v
@@ -83,8 +83,32 @@ Fixpoint transf_stmt (s : Emajor.stmt) (fresh : ident) : (Dmajor.stmt * ident) :
        Pos.succ fresh)
   end.
 
-(* Eval simpl in (transf_stmt (SmakeConstr xH 4 (Emajor.Var xH :: Emajor.Var xH :: nil)) xH). *)
-(* Eval simpl in (transf_stmt (SmakeClose xH xH (Emajor.Var xH :: Emajor.Var xH :: nil)) xH). *)
+Definition transf_fun_body (s : Emajor.stmt) (e : Emajor.expr) : Dmajor.stmt :=
+  let (bod,_) := transf_stmt s xH in
+  let ret := Dmajor.Sreturn (Some (transf_expr e)) in
+  bod; ret.
+
+Definition transf_function (f : Emajor.function) : Dmajor.function :=
+  let (s,e) := Emajor.fn_body f in
+  let ts := transf_fun_body s e in
+  let ss := Emajor.fn_stackspace f in
+  let params := Emajor.fn_params f in
+  Dmajor.mkfunction EMsig params nil ss ts.
+
+Definition transf_fundef (fd : Emajor.fundef) : Dmajor.fundef :=
+  transf_function fd.
+
+Definition transf_prog (p : Emajor.program) : Dmajor.program :=
+  AST.transform_program transf_fundef p.
+
+Section PRESERVATION.
+
+Variable prog: Emajor.program.
+Variable tprog: Dmajor.program.
+Let ge := Genv.globalenv prog.
+Let tge := Genv.globalenv tprog.
+Hypothesis TRANSF : transf_prog prog = tprog.
+
 
 
 (* given an address, addresses of the nested values *)
@@ -113,7 +137,7 @@ Fixpoint load_all (l : list (value * val)) (m : mem) : option (list (value * val
 
 (* mapping of high level values to low level values *)
 (* everything is one pointer *)
-Inductive value_inject (ge : genv) (m : mem) : value -> val -> Prop :=
+Inductive value_inject (m : mem) : value -> val -> Prop :=
 | inj_constr :
     (* a constructor is a pointer to the correct tag *)
     (* and every value following that in memory is a value for that constructor *)
@@ -122,23 +146,23 @@ Inductive value_inject (ge : genv) (m : mem) : value -> val -> Prop :=
     forall b ofs n values l',
       Mem.loadv Mint32 m (Vptr b ofs) = Some (Vint n) -> (* correct tag *)
       load_all (arg_addrs b ofs values) m = Some l' -> (* one more deref for args *)
-      (forall a b, In (a,b) l' -> value_inject ge m a b) -> (* all args inject *)
-      value_inject ge m (Constr (Int.unsigned n) values) (Vptr b ofs)
+      (forall a b, In (a,b) l' -> value_inject m a b) -> (* all args inject *)
+      value_inject m (Constr (Int.unsigned n) values) (Vptr b ofs)
 | inj_closure :
     forall b ofs bcode f fname values l',
       Mem.loadv Mint32 m (Vptr b ofs) = Some (Vptr bcode Int.zero) ->
-      Genv.find_funct_ptr ge bcode = Some f -> (* legit pointer to some code *)
-      Genv.find_symbol ge fname = Some bcode -> (* name we have points to same code *)
+      Genv.find_funct_ptr tge bcode = Some f -> (* legit pointer to some code *)
+      Genv.find_symbol tge fname = Some bcode -> (* name we have points to same code *)
       load_all (arg_addrs b ofs values) m = Some l' -> (* one more deref for args *)
-      (forall a b, In (a,b) l' -> value_inject ge m a b) -> (* all args inject *)
-      value_inject ge m (Close fname values) (Vptr b ofs).
+      (forall a b, In (a,b) l' -> value_inject m a b) -> (* all args inject *)
+      value_inject m (Close fname values) (Vptr b ofs).
 
 
-Definition env_inject (Ee : Emajor.env) (De : Dmajor.env) (ge : genv) (m : mem) : Prop :=
+Definition env_inject (Ee : Emajor.env) (De : Dmajor.env) (m : mem) : Prop :=
   forall id v,
     PTree.get id Ee = Some v ->
     exists v',
-      PTree.get id De = Some v' /\ value_inject ge m v v'.
+      PTree.get id De = Some v' /\ value_inject m v v'.
   
 
 Lemma load_all_val :
@@ -179,12 +203,12 @@ Proof.
 Admitted. 
 
 Lemma transf_expr_inject :
-  forall Ee De ge m sp,
-    env_inject Ee De ge m ->
+  forall Ee De m sp,
+    env_inject Ee De m ->
     forall (exp : Emajor.expr) v,
       Emajor.eval_expr Ee exp v ->
       exists v',
-        Dmajor.eval_expr ge De m sp (transf_expr exp) v' /\ value_inject ge m v v'.
+        Dmajor.eval_expr tge De m sp (transf_expr exp) v' /\ value_inject m v v'.
 Proof.
   induction exp; intros.
   * inv H0. unfold env_inject in H.
@@ -226,3 +250,134 @@ Proof.
       (Val.add (Vptr b ofs) (Vint (Int.repr (4 + 4 * Z.of_nat n)))) by (simpl; reflexivity).
       repeat (econstructor; eauto).
 Qed.      
+
+
+Inductive match_cont: Emajor.cont -> Dmajor.cont -> Prop :=
+| match_cont_stop:
+    match_cont Emajor.Kstop Dmajor.Kstop
+| match_cont_block :
+    forall k k',
+      match_cont k k' ->
+      match_cont (Emajor.Kblock k) (Dmajor.Kblock k')
+| match_cont_seq: forall s s' k k',
+    (exists id id', transf_stmt s id = (s',id')) ->
+    match_cont k k' ->
+    match_cont (Emajor.Kseq s k) (Dmajor.Kseq s' k')
+| match_cont_call: forall id f sp e k f' e' k' m expr,
+    (* expr is unconstrained, probably not right *)
+    (* TODO: rewrite here when expr constraints figured out *)
+    transf_function f = f' ->
+    match_cont k k' ->
+    env_inject e e' m ->
+    match_cont (Emajor.Kcall id expr f e k) (Dmajor.Kcall (Some id) f' sp e' k') .
+
+
+Inductive match_states: Emajor.state -> Dmajor.state -> Prop :=
+| match_state :
+    forall f f' s s' expr k k' e e' sp m,
+      transf_function f = f' ->
+      (exists id id', transf_stmt s id = (s',id')) ->
+      match_cont k k' ->
+      env_inject e e' m ->
+      match_states (Emajor.State f s expr k e) (Dmajor.State f' s' k' sp e' m)
+| match_callstate :
+    forall fd fd' vals vals' m k k',
+      transf_fundef fd = fd' ->
+      list_forall2 (value_inject m) vals vals' ->
+      match_cont k k' ->
+      match_states (Emajor.Callstate fd vals k) (Dmajor.Callstate fd' vals' k' m)
+| match_returnstate :
+    forall v v' k k' m,
+      value_inject m v v' ->
+      match_cont k k' ->
+      match_states (Emajor.Returnstate v k) (Dmajor.Returnstate v' k' m).
+
+Remark call_cont_commut:
+  forall k k', match_cont k k' -> match_cont (Emajor.call_cont k) (Dmajor.call_cont k').
+Proof.
+  induction 1; simpl; auto. constructor. econstructor; eauto.
+Qed.
+
+Lemma is_call_cont_transf :
+  forall k k',
+    Emajor.is_call_cont k ->
+    match_cont k k' ->
+    Dmajor.is_call_cont k'.
+Proof.
+  intros. destruct k; simpl in *; try solve [inv H]; inv H0; eauto.
+Qed.
+
+
+
+Lemma find_symbol_transf :
+  forall id,
+    Genv.find_symbol tge id = Genv.find_symbol ge id.
+Proof.
+  intros. unfold tge.
+  unfold ge. rewrite <- TRANSF.
+  apply Genv.find_symbol_transf.
+Qed.
+
+(* number of bytes to store a value *)
+Definition size_bytes (v : value) : Z :=
+  match v with
+  | Close _ l => (4 * Z.of_nat (length l)) + 4
+  | Constr _ l => (4 * Z.of_nat (length l)) + 4
+  end.
+
+
+Definition first_byte (v : value) : option val :=
+  match v with
+  | Close fname _ =>
+    match Genv.find_symbol tge fname with
+    | Some b => Some (Vptr b Int.zero)
+    | None => None
+    end
+  | Constr tag _ => Some (Vint (Int.repr tag))
+  end.
+
+Definition rest (v : value) : list value :=
+  match v with
+  | Close _ l => l
+  | Constr _ l => l
+  end.
+
+Fixpoint store_value (v : value) (m : mem) {struct v} : option (val * mem) :=
+  let sz := size_bytes v in
+  let (m',b) := Mem.alloc m 0 sz in
+  match first_byte v with
+  | None => None
+  | Some vl =>
+    match Mem.storev Mint32 m' (Vptr b Int.zero) vl with
+    | Some m'' =>
+      match store_values b (4%Z) (rest v) m'' with
+      | Some m''' => 
+        Some (Vptr b Int.zero, m''')
+      | None => None
+      end
+    | None => None
+    end
+  end
+
+with store_values (b : block) (ofs : Z) (l : list value) (m : mem) {struct l} : option mem :=
+       match l with
+       | nil => Some m
+       | v :: vs =>
+         match store_values b (ofs + 4)%Z vs m with
+         | Some m' =>
+           match store_value v m' with
+           | Some (vp,m'') => Mem.storev Mint32 m'' (Vptr b (Int.repr ofs)) vp
+           | None => None
+           end
+         | None => None
+         end
+end.
+
+      
+
+(* This is the cool one *)
+(* if we Set in Emajor land, what operation is that in Dmajor? *)
+Lemma env_inject_set :
+  forall e e' m,
+    env_inject m e e' ->
+    
