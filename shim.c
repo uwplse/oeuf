e7db520d815aca058133695a5496e15110b5c660
Stuart Pernsteiner <spernste@cs.washington.edu>
2016-06-06 14:26:18 -0700

    add shim.c

---
+++ shim.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+union nat {
+    int tag;
+    struct {
+        int tag;
+    } O;
+    struct {
+        int tag;
+        union nat* n;
+    } S;
+};
+
+union nat* make_nat(int n) {
+    union nat* ptr = malloc(4);
+    ptr->tag = 0;
+
+    for (int i = 1; i <= n; ++i) {
+        union nat* tmp = malloc(8);
+        tmp->tag = 1;
+        tmp->S.n = ptr;
+        ptr = tmp;
+    }
+
+    return ptr;
+}
+
+int read_nat(union nat* n) {
+    int i = 0;
+    while (n->tag == 1) {
+        ++i;
+        n = n->S.n;
+    }
+    return i;
+}
+
+int main() {
+    union nat* one = make_nat(1);
+    union nat* two = make_nat(2);
+
+    printf("%d + %d = %d\n", read_nat(one), read_nat(two), 999);
+
+    return 0;
+}
+
