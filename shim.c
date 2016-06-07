0482dbb3149599d21b7053a9024527365627af21
Stuart Pernsteiner <spernste@cs.washington.edu>
2016-06-06 20:52:39 -0700

    make shim call into oeuf.o

--- shim.c
+++ shim.c
@@ -35,16 +35,28 @@ int read_nat(union nat* n) {
     return i;
 }
 
+
+struct closure {
+    void (*f)(void*, void*);
+};
+
+void* call(void* f, void* a) {
+    (((struct closure*)f)->f)(f, a);
+}
+
 // union nat* add(union nat* a, union nat* b);
+
+extern void* _$8(void*, void*);
 
 int main() {
     union nat* one = make_nat(1);
     union nat* two = make_nat(2);
 
-    // union nat* result = add(one, two);
-    // printf("%d + %d = %d\n", read_nat(one), read_nat(two), read_nat(result));
+    void* add = _$8(NULL, NULL);
+    void* tmp1 = call(add, one);
+    void* tmp2 = call(tmp1, two);
 
-    printf("%d + %d = %d\n", read_nat(one), read_nat(two), 999);
+    printf("%d + %d = %d\n", read_nat(one), read_nat(two), read_nat(tmp2));
 
     return 0;
 }
