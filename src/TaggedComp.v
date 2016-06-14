4ac248658058db3f5c9b6755bbd99804f3800b4f
Stuart Pernsteiner <spernste@cs.washington.edu>
2016-06-14 14:02:58 -0700

    progress on TaggedComp.match_states

--- src/TaggedComp.v
+++ src/TaggedComp.v
@@ -241,15 +241,182 @@ Inductive match_states (LE : L.env) (TE : T.env) : L.expr -> T.expr -> Prop :=
         match_states LE TE (L.Close fn largs) (T.Close fn targs)
 .
 
+
+Lemma Forall_nth_error : forall X (P : X -> Prop) xs,
+    Forall P xs ->
+    (forall i x, nth_error xs i = Some x -> P x).
+intros.
+rewrite Forall_forall in *.
+eauto using nth_error_In.
+Qed.
+
+Lemma nth_error_Forall : forall X (P : X -> Prop) xs,
+    (forall i x, nth_error xs i = Some x -> P x) ->
+    Forall P xs.
+intros. rewrite Forall_forall in *. intros.
+fwd eapply In_nth_error; eauto. break_exists. eauto.
+Qed.
+
+
+Lemma Forall2_length : forall X Y (P : X -> Y -> Prop) xs ys,
+    Forall2 P xs ys ->
+    length xs = length ys.
+induction xs; intros0 Hfa; invc Hfa; simpl; eauto.
+Qed.
+
+Lemma Forall2_nth_error : forall X Y (P : X -> Y -> Prop) xs ys,
+    Forall2 P xs ys ->
+    (forall i x y,
+        nth_error xs i = Some x ->
+        nth_error ys i = Some y ->
+        P x y).
+induction xs; intros0 Hfa; invc Hfa; intros0 Hnx Hny.
+- destruct i; discriminate Hnx.
+- destruct i; simpl in Hnx, Hny.
+  + congruence.
+  + eapply IHxs; eauto.
+Qed.
+
+Lemma nth_error_Forall2 : forall X Y (P : X -> Y -> Prop) xs ys,
+    length xs = length ys ->
+    (forall i x y,
+        nth_error xs i = Some x ->
+        nth_error ys i = Some y ->
+        P x y) ->
+    Forall2 P xs ys.
+induction xs; intros0 Hlen Hnth; destruct ys; invc Hlen.
+- constructor.
+- constructor.
+  + eapply Hnth with (i := 0); reflexivity.
+  + eapply IHxs; eauto.
+    intros. eapply Hnth with (i := S i); simpl; eauto.
+Qed.
+
+Lemma length_nth_error_Some : forall X Y  xs ys x i,
+    @length X xs = @length Y ys ->
+    nth_error xs i = Some x ->
+    exists y, nth_error ys i = Some y.
+intros.
+destruct (nth_error ys i) eqn:?; try eauto.
+rewrite nth_error_None in *.
+assert (nth_error xs i <> None) by congruence.
+rewrite nth_error_Some in *.
+omega.
+Qed.
+
+Lemma Forall2_nth_error_Some : forall X Y (P : X -> Y -> Prop) xs ys x i,
+    Forall2 P xs ys ->
+    nth_error xs i = Some x ->
+    exists y, nth_error ys i = Some y.
+intros. eapply length_nth_error_Some. eapply Forall2_length. all:eauto.
+Qed.
+
+Lemma Forall2_flip : forall A B (P : A -> B -> Prop) (Q : B -> A -> Prop) (xs : list A) (ys : list B),
+    (forall x y, P x y -> Q y x) ->
+    Forall2 P xs ys -> Forall2 Q ys xs.
+induction xs; intros0 Hpq Hfa; invc Hfa; constructor; firstorder eauto.
+Qed.
+
+Lemma Forall2_flip' : forall A B (P : A -> B -> Prop) (xs : list A) (ys : list B),
+    Forall2 P xs ys -> Forall2 (fun y x => P x y) ys xs.
+intros; eapply Forall2_flip; eauto.
+Qed.
+
+
+
+
+Ltac collect_length_hyps :=
+    repeat match goal with
+    | [ H : Forall2 _ ?xs_ ?ys_ |- _ ] =>
+            lazymatch goal with
+            | [ H : length xs_ = length ys_ |- _ ] => idtac (* already known *)
+            | [ |- _ ] => 
+                    fwd eapply Forall2_length with (xs := xs_) (ys := ys_) (1 := H)
+            end
+    end.
+
+Ltac find_matching_entries HH i :=
+    repeat match type of HH with
+    | forall y, nth_error ?ys_ i = Some y -> _ =>
+            first
+                [ specialize (HH ?? **)
+                | let Hexist := fresh "H" in
+                  let y := fresh "y" in
+                  let Hy := fresh "H" y in
+                  fwd eapply length_nth_error_Some with (ys := ys_) as Hexist;
+                  [ | eassumption | ];
+                  [ congruence | ];
+                  destruct Hexist as [y Hy];
+                  specialize (HH y Hy) ]
+    end.
+
+Ltac require_no_match P :=
+    lazymatch goal with
+    | [ H : P |- _ ] => fail "expected no hypothesis matching" P ", but found" H
+    | [ |- _ ] => idtac
+    end.
+
+Ltac collect_entry_hyps i :=
+    repeat match goal with
+    | [ Hfa : Forall ?P ?xs, Hnth : nth_error ?xs i = Some ?x |- _ ] =>
+            require_no_match (P x);
+            assert (P x) by (eapply Forall_nth_error with (1 := Hfa) (2 := Hnth))
+    | [ Hfa : Forall2 ?P ?xs ?ys,
+        Hnx : nth_error ?xs i = Some ?x,
+        Hny : nth_error ?ys i = Some ?y |- _ ] =>
+            require_no_match (P x y);
+            assert (P x y) by
+                (eapply Forall2_nth_error with (1 := Hfa) (2 := Hnx) (3 := Hny))
+    end.
+
+Ltac list_magic HH :=
+    let i := fresh "i" in
+    (eapply nth_error_Forall || eapply nth_error_Forall2);
+    intro i; intros;
+    specialize (HH i);
+    collect_length_hyps; find_matching_entries HH i; collect_entry_hyps i;
+    eapply HH; eauto.
+
+
+
+
+
+
+
 Lemma match_value : forall LE TE le te,
     match_states LE TE le te ->
     L.value le ->
     T.value te.
 induction le using L.expr_ind'; intros0 Hmatch Lval; invc Lval; invc Hmatch.
 
-- admit.
-- admit.
-Admitted.
+- constructor.
+
+  assert (HH :
+  forall i,
+      forall arg, nth_error args i = Some arg ->
+      forall targ, nth_error targs i = Some targ ->
+      (forall te, match_states LE TE arg te -> L.value arg -> T.value te) ->
+      L.value arg ->
+      match_states LE TE arg targ ->
+      T.value targ
+  ) by (clear; intros; eauto).
+
+  list_magic **.
+
+- constructor.
+
+  assert (HH :
+  forall i,
+      forall arg, nth_error free i = Some arg ->
+      forall targ, nth_error targs i = Some targ ->
+      (forall te, match_states LE TE arg te -> L.value arg -> T.value te) ->
+      L.value arg ->
+      match_states LE TE arg targ ->
+      T.value targ
+  ) by (clear; intros; eauto).
+
+  list_magic **.
+Qed.
 
 Theorem match_step : forall LE TE le le' te,
     Forall2 (match_states LE TE) LE TE ->
@@ -266,9 +433,17 @@ induction le using L.expr_ind'; intros0 Henv Hmatch Lstep; try solve [invc Lstep
     invc Hmatch. invc H4.
 
     eexists. split.
-    -- eapply T.MakeCall; eauto using match_value.
-       ++ admit. (* need good lemmas about Forall/Forall2 *)
-       ++ admit. (* because length LE = length TE *)
+    -- fwd eapply Forall2_nth_error_Some with (xs := LE) (ys := TE); eauto.  break_exists.
+       eapply T.MakeCall; eauto using match_value.
+
+       ++ assert (forall i,
+            forall lv, nth_error free i = Some lv ->
+            forall tv, nth_error targs i = Some tv ->
+            match_states LE TE lv tv ->
+            L.value lv ->
+            T.value tv) by (eauto using match_value).
+          list_magic **.
+
        ++ admit. (* T.subst succeeds because L.subst succeeds *)
 
     -- admit. (* (1) needs subst/match lemma; (2) need to know how te' was made *)
