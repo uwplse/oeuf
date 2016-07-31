26428d177a022cf50d947c49cc3a07cc08e93abf
Zach Tatlock <ztatlock@cs.washington.edu>
2016-07-31 14:03:08 -0700

    simple test script

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,49 @@
+#!/usr/bin/env bash
 
-echo "No Tests Currently"
+COMP="./OeufDriver.native"
+
+TESTS="
+  fib.oeuf
+  add.oeuf
+  echo_initial_state.oeuf
+  echo_handleInput.oeuf
+  echo_handleMsg.oeuf
+  "
+
+# ANSI color codes
+BK=$'\033[1;30m' # black
+RD=$'\033[1;31m' # red
+GR=$'\033[1;32m' # green
+YL=$'\033[1;33m' # yellow
+BL=$'\033[1;34m' # blue
+MG=$'\033[1;35m' # magenta
+CY=$'\033[1;36m' # cyan
+WT=$'\033[1;37m' # white
+NC=$'\033[0m'    # no color
+
+PASS="${GR}PASS${NC}"
+FAIL="${RD}FAIL${NC}"
+
+if ! [ -f "$COMP" ]; then
+  cat <<EOF
+ERROR: "$COMP" does not exist.
+
+Please build "$COMP" by running:
+  make compcert
+  make proof
+  make driver
+EOF
+  exit 1
+fi
+
+for t in $TESTS; do
+  printf "%-25s" "$t"
+  out="$(basename "$t" .oeuf).o"
+  "$COMP" -c "$t"
+  if [ -f "$out" ]; then
+    echo "$PASS"
+    rm "$out"
+  else
+    echo "$FAIL"
+  fi
+done
