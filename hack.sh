84c91b19381723c2a7901783c442a95b628c367c
Eric A Mullen <emullen@cs.washington.edu>
2016-08-12 11:22:40 -0700

    fixed segfault

--- hack.sh
+++ hack.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-sed -i -e '/\.size/d' -e '/\.type/d' -e 's/_\$/__$/' -e 's/malloc/_malloc/' oeuf.s
+gsed -i -e '/\.size/d' -e '/\.type/d' -e 's/_\$/__$/' -e 's/malloc/_malloc/' oeuf.s
 
 gcc -m32 -o oeuf oeuf.s shim.c -Wl,-no_pie
