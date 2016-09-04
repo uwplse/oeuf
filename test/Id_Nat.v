b859e15048c11b88df5f97b07863c972ca2f2268
James Wilcox <jrw12@cs.washington.edu>
2016-09-03 22:41:43 -0700

    update tests to use names

--- test/Id_Nat.v
+++ test/Id_Nat.v
@@ -2,4 +2,4 @@ Require List SourceLang Pretty OeufPlugin.OeufPlugin CompilationUnit.
 Import List.ListNotations.
 
 Eval compute Then Write To File "id_nat.oeuf"
-     (Pretty.compilation_unit.print (CompilationUnit.singleton (@SourceLang.id_nat_reflect []))).
+     (Pretty.compilation_unit.print (CompilationUnit.singleton (@SourceLang.id_nat_reflect []) "id_nat")).
--- test/MultipleSymbols.v
+++ test/MultipleSymbols.v
@@ -1,7 +1,9 @@
 Require List SourceLang Pretty OeufPlugin.OeufPlugin CompilationUnit HList.
 Import HList List.ListNotations.
+Require Import String.
 
 Eval lazy Then Write To File "multiplesymbols.oeuf"
      (Pretty.compilation_unit.pretty 75
        (CompilationUnit.CompilationUnit _ (hcons (@SourceLang.add_reflect [])
-                                          (hcons (@SourceLang.fib_reflect []) hnil)))).
+                                          (hcons (@SourceLang.fib_reflect []) hnil))
+                                        ["add"; "fib"]%list%string)).
