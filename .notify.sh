59fa663559efcedc4452de56d788d997cfb3ff37
Eric A Mullen <emullen@cs.washington.edu>
2016-09-28 11:52:31 -0700

    broke the build, to test the build bot notifier

--- .notify.sh
+++ .notify.sh
@@ -1,3 +1,8 @@
-echo "PLACEHOLDER FOR SEND EMAIL"
+echo "sending email about broken build"
+
+cat $1 | mail -s "[OEUF BUILD BOT] Build is Broken" emullen@cs.washington.edu
+cat $1 | mail -s "[OEUF BUILD BOT] Build is Broken" jrw12@cs.washington.edu
+cat $1 | mail -s "[OEUF BUILD BOT] Build is Broken" spernste@cs.washington.edu
+
 
 
