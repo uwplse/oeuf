ae55fdf9a0b758fe5b9557dad0c96f1155f00556
Eric A Mullen <emullen@cs.washington.edu>
2016-09-28 12:14:09 -0700

    broken build now posts in slack

--- .notify.sh
+++ .notify.sh
@@ -1,8 +1,14 @@
-echo "sending email about broken build"
+echo "sending notifications about broken build"
 
-cat $1 | mail -s "[OEUF BUILD BOT] Build is Broken" emullen@cs.washington.edu
-cat $1 | mail -s "[OEUF BUILD BOT] Build is Broken" jrw12@cs.washington.edu
-cat $1 | mail -s "[OEUF BUILD BOT] Build is Broken" spernste@cs.washington.edu
+#Send us email
+#I don't think this works on warfa, so I'm leaving it disabled
+# cat $1 | mail -s "[OEUF BUILD BOT] Build is Broken" emullen@cs.washington.edu
+# cat $1 | mail -s "[OEUF BUILD BOT] Build is Broken" jrw12@cs.washington.edu
+# cat $1 | mail -s "[OEUF BUILD BOT] Build is Broken" spernste@cs.washington.edu
 
+#Post in slack
+curl -sf -XPOST \
+     --data-urlencode "payload={\"channel\":\"#oeuf\",\"link_names\":1,\"text\":\"$(python -c 'import sys; print(sys.argv[1].replace("\"", "\\\""))' "$1")\"}" \
+     'https://hooks.slack.com/services/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'
 
 
