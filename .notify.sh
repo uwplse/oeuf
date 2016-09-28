bfe27a4b3346ca9303a08ed96fc2c19d07a930cd
Eric A Mullen <emullen@cs.washington.edu>
2016-09-28 14:08:03 -0700

    can't send mail, idk why

--- .notify.sh
+++ .notify.sh
@@ -1,13 +1,14 @@
 echo "sending notifications about broken build"
 
 #Send us email
-cat $1 | mail -s "[OEUF BUILD BOT] Build is Broken" emullen@cs.washington.edu
-cat $1 | mail -s "[OEUF BUILD BOT] Build is Broken" jrw12@cs.washington.edu
-cat $1 | mail -s "[OEUF BUILD BOT] Build is Broken" spernste@cs.washington.edu
+echo "Broken Build" | mail -s "[OEUF BUILD BOT] Build is Broken" emullen@cs.washington.edu
+# cat $1 | mail -s "[OEUF BUILD BOT] Build is Broken" jrw12@cs.washington.edu
+# cat $1 | mail -s "[OEUF BUILD BOT] Build is Broken" spernste@cs.washington.edu
 
 
 
 #Post in slack
+#credit to Calvin for this
 curl -sf -XPOST \
      --data-urlencode "payload={\"channel\":\"#oeuf\",\"link_names\":1,\"text\":\"$(python -c 'import sys; print(sys.argv[1].replace("\"", "\\\""))' "Build Broken")\"}" \
      'https://hooks.slack.com/services/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'
