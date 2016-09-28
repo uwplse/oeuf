4d02d3d1a1cae42bd799fc8bdc1dd88d6dfe87c7
Eric A Mullen <emullen@cs.washington.edu>
2016-09-28 13:27:12 -0700

    hopefully works better

--- .notify.sh
+++ .notify.sh
@@ -5,9 +5,11 @@ cat $1 | mail -s "[OEUF BUILD BOT] Build is Broken" emullen@cs.washington.edu
 cat $1 | mail -s "[OEUF BUILD BOT] Build is Broken" jrw12@cs.washington.edu
 cat $1 | mail -s "[OEUF BUILD BOT] Build is Broken" spernste@cs.washington.edu
 
+
+
 #Post in slack
 curl -sf -XPOST \
-     --data-urlencode "payload={\"channel\":\"#oeuf\",\"link_names\":1,\"text\":\"$(python -c 'import sys; print(sys.argv[1].replace("\"", "\\\""))' "$1")\"}" \
+     --data-urlencode "payload={\"channel\":\"#oeuf\",\"link_names\":1,\"text\":\"$(python -c 'import sys; print(sys.argv[1].replace("\"", "\\\""))' "Build Broken")\"}" \
      'https://hooks.slack.com/services/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'
 
 
