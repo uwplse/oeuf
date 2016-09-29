30c30e2428925c1c6cea65c4c252b539bd825ef2
Eric A Mullen <emullen@cs.washington.edu>
2016-09-29 16:47:05 -0700

    Zach liked this syntax better

--- test.sh
+++ test.sh
@@ -9,8 +9,7 @@ COQC="coqc $(sed '/^[[:space:]]*$/q' _CoqProject)"
 
 PASSED=true
 
-for f in test/*.v
-do
+for f in test/*.v ; do
 # unfortunately this does not correctly handle quoting from _CoqProject,
 # so those rules that are necessary are repeated here
     $COQC -Q src "" "$f" >/dev/null 2>&1 || { echo "coqc failed on $f"; exit 1; }
