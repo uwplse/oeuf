cb1edbeb3f6ab6ceb610decb52bdb62374ea7e34
Eric A Mullen <emullen@cs.washington.edu>
2016-10-03 17:23:09 -0700

    now makes metrics which are valid json

--- make_metrics.sh
+++ make_metrics.sh
@@ -9,12 +9,19 @@ touch $METRICS
 
 echo "[" >> $METRICS
 
+FIRST="1"
+
 for f in src/*.v ; do
 
+    if [[ $FIRST == "1" ]] ; then
+	FIRST=0
+    else
+	echo "," >> $METRICS
+    fi
     cat >> $METRICS <<EOF
- { "name" : $f
+ { "name" : "$f"
  , "count" : $(grep -i "admit" $f | wc -l)
- },
+ }
 EOF
     
 done
