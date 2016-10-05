4c0dc2d4a568ce854805f218e98152e5d9211f8a
Stuart Pernsteiner <spernste@cs.washington.edu>
2016-10-05 15:29:13 -0700

    work on ElimFuncComp2 match_states

--- src/ElimFuncComp2.v
+++ src/ElimFuncComp2.v
@@ -22,8 +22,8 @@ Definition free_list n :=
     | S n => free_list' [] n
     end.
 
-Fixpoint close_dyn_free drop expect :=
-    skipn drop (free_list expect).
+Definition close_dyn_free drop expect :=
+    skipn drop (free_list (expect + drop)).
 
 
 Definition compile :=
@@ -84,96 +84,320 @@ Definition compile_cu (cu : list A.expr * list metadata) : list B.expr * list me
 
 
 
-Inductive I_expr : A.expr -> B.expr -> Prop :=
-| IArg : I_expr A.Arg B.Arg
-| IUpVar : forall n, I_expr (A.UpVar n) (B.UpVar n)
+Inductive I_expr (AE : A.env) (BE : B.env) : A.expr -> B.expr -> Prop :=
+| IArg : I_expr AE BE A.Arg B.Arg
+| IUpVar : forall n, I_expr AE BE (A.UpVar n) (B.UpVar n)
 | ICall : forall af aa bf ba,
-        I_expr af bf ->
-        I_expr aa ba ->
-        I_expr (A.Call af aa) (B.Call bf ba)
+        I_expr AE BE af bf ->
+        I_expr AE BE aa ba ->
+        I_expr AE BE (A.Call af aa) (B.Call bf ba)
 | IConstr : forall tag aargs bargs,
-        Forall2 I_expr aargs bargs ->
-        I_expr (A.Constr tag aargs) (B.Constr tag bargs)
+        Forall2 (I_expr AE BE) aargs bargs ->
+        I_expr AE BE (A.Constr tag aargs) (B.Constr tag bargs)
 | IElimBody : forall arec acases brec bcases,
-        I_expr arec brec ->
-        Forall2 (fun ap bp => I_expr (fst ap) (fst bp)) acases bcases ->
-        I_expr (A.ElimBody arec acases) (B.ElimBody brec bcases)
-| IClose : forall tag afree bfree,
-        Forall2 I_expr afree bfree ->
-        I_expr (A.Close tag afree) (B.Close tag bfree)
+        I_expr AE BE arec brec ->
+        Forall2 (fun ap bp => I_expr AE BE (fst ap) (fst bp)) acases bcases ->
+        I_expr AE BE (A.ElimBody arec acases) (B.ElimBody brec bcases)
+| IClose1 : forall fname afree bfree,
+        Forall2 (I_expr AE BE) afree bfree ->
+        I_expr AE BE (A.Close fname afree) (B.Close fname bfree)
+| IClose2 : forall fname afree bfree n body,
+        nth_error BE fname = Some body ->
+        B.num_locals body <= n ->
+        Forall2 (I_expr AE BE) (firstn n afree) bfree ->
+        (* Additional constraints ensure we avoid matching non-values to values.
+           This case only occurs after evaluating a CloseDyn, so it's okay. *)
+        Forall A.value afree ->
+        Forall B.value bfree ->
+        I_expr AE BE (A.Close fname afree) (B.Close fname bfree)
 | ICloseDyn : forall fname adrop aexpect bfree,
         close_dyn_free adrop aexpect = bfree ->
-        I_expr (A.CloseDyn fname adrop aexpect)
-               (B.Close fname bfree)
+        aexpect > 0 ->
+        I_expr AE BE (A.CloseDyn fname adrop aexpect)
+                     (B.Close fname bfree)
+(* Special case for `Call (CloseDyn _ _ 0) _`.  Otherwise we end up matching
+ * non-values to values, and all hell breaks loose. *)
+| ICallCdzExpr : forall fname adrop aarg barg,
+        I_expr AE BE aarg barg ->
+        I_expr AE BE (A.Call (A.CloseDyn fname adrop 0) aarg)
+                     (B.Call (B.Close fname []) barg)
 .
 
-Inductive I : A.state -> B.state -> Prop :=
+Inductive I (AE : A.env) (BE : B.env) : A.state -> B.state -> Prop :=
 | IRun : forall ae al ak be bl bk,
-        I_expr ae be ->
+        I_expr AE BE ae be ->
         Forall A.value al ->
         Forall B.value bl ->
         B.num_locals be <= length bl ->
-        Forall2 I_expr (firstn (length bl) al) bl ->
+        Forall2 (I_expr AE BE) (firstn (length bl) al) bl ->
         (forall av bv,
             A.value av ->
             B.value bv ->
-            I_expr av bv ->
-            I (ak av) (bk bv)) ->
-        I (A.Run ae al ak) (B.Run be bl bk)
+            I_expr AE BE av bv ->
+            I AE BE (ak av) (bk bv)) ->
+        I AE BE (A.Run ae al ak) (B.Run be bl bk)
+
+(* Special hack for the intermediate state in `Call (CloseDyn _ _ 0) _`
+
+   On the left, 2 steps:                      On the right, no steps:
+         Call (CloseDyn _ _ 0) _        <=>     Call (Close _ [])
+      -> CloseDyn _ _ 0  [k := ...]     <=>     ???
+      -> Call (Close _ []) _            <=>     Call (Close _ [])
+
+   This constructor handles the middle case.
+ *)
+| ICallCdz : forall fname drop aarg al ak barg bl bk,
+        I_expr AE BE aarg barg ->
+        Forall A.value al ->
+        Forall B.value bl ->
+        B.num_locals barg <= length bl ->
+        Forall2 (I_expr AE BE) (firstn (length bl) al) bl ->
+        (forall av bv,
+            A.value av ->
+            B.value bv ->
+            I_expr AE BE av bv ->
+            I AE BE (ak av) (bk bv)) ->
+        I AE BE (A.Run (A.CloseDyn fname drop 0) al
+                    (fun av => A.Run (A.Call av aarg) al ak))
+                (B.Run (B.Call (B.Close fname []) barg) bl bk)
+
 | IStop : forall ae be,
-        I_expr ae be ->
-        I (A.Stop ae) (B.Stop be).
+        I_expr AE BE ae be ->
+        I AE BE (A.Stop ae) (B.Stop be).
 
 
 
-Lemma compile_I_expr : forall ae be,
-    compile ae = be ->
-    I_expr ae be.
-induction ae using A.expr_rect_mut with
-    (Pl := fun aes => forall bes,
-        compile_list aes = bes ->
-        Forall2 I_expr aes bes)
-    (Pp := fun ap => forall bp,
-        compile_pair ap = bp ->
-        I_expr (fst ap) (fst bp))
-    (Plp := fun aps => forall bps,
-        compile_list_pair aps = bps ->
-        Forall2 (fun ap bp => I_expr (fst ap) (fst bp)) aps bps);
-intros0 Hcomp; simpl in Hcomp; refold_compile; try rewrite <- Hcomp;
-try solve [eauto | constructor; eauto].
+(* `CloseDyn _ _ 0` needs all sorts of special handling, because it `compile`s
+ * from non-value to value (`Close _ []` is always a value) *)
+Inductive is_close_dyn_zero : A.expr -> Prop :=
+| IsCloseDynZero : forall fname drop, is_close_dyn_zero (A.CloseDyn fname drop 0).
+
+Definition is_close_dyn_zero_dec (e : A.expr) :
+    { is_close_dyn_zero e } + { ~ is_close_dyn_zero e }.
+destruct e; try solve [right; inversion 1].
+destruct expect; try solve [right; inversion 1].
+left. constructor.
 Qed.
 
-Lemma I_expr_value : forall a b,
-    I_expr a b ->
+Definition is_close_dyn_zero_state s :=
+    match s with
+    | A.Run e _ _ => is_close_dyn_zero e
+    | A.Stop e => is_close_dyn_zero e
+    end.
+
+
+Inductive almost_value : A.expr -> Prop :=
+| AvConstr : forall tag args,
+        Forall almost_value args ->
+        almost_value (A.Constr tag args)
+| AvClose : forall fname free,
+        Forall almost_value free ->
+        almost_value (A.Close fname free)
+| AvCloseDynZero : forall fname drop,
+        almost_value (A.CloseDyn fname drop 0).
+
+Lemma value_almost_value : forall v, A.value v -> almost_value v.
+induction v using A.expr_ind''; inversion 1.
+- constructor. list_magic_on (args, tt).
+- constructor. list_magic_on (free, tt).
+Qed.
+
+
+
+Definition A_close_dyn_placement :=
+    let fix go e :=
+        let fix go_list es :=
+            match es with
+            | [] => True
+            | e :: es => go e /\ go_list es
+            end in
+        let fix go_pair p :=
+            let '(e, r) := p in
+            go e in
+        let fix go_list_pair ps :=
+            match ps with
+            | [] => True
+            | p :: ps => go_pair p /\ go_list_pair ps
+            end in
+        match e with
+        | A.Arg => True
+        | A.UpVar _ => True
+        | A.Call (A.CloseDyn _ _ _) a => go a
+        | A.Call f a => go f /\ go a
+        | A.Constr _ args => go_list args
+        | A.ElimBody rec cases => go rec /\ go_list_pair cases
+        | A.Close _ free => go_list free
+        | A.CloseDyn _ _ _ => False
+        end in go.
+
+Definition A_close_dyn_placement_list :=
+    let go := A_close_dyn_placement in
+    let fix go_list es :=
+        match es with
+        | [] => True
+        | e :: es => go e /\ go_list es
+        end in go_list.
+
+Definition A_close_dyn_placement_pair :=
+    let go := A_close_dyn_placement in
+    let fix go_pair (p : A.expr * A.rec_info) :=
+        match p with
+        | (e, _) => go e
+        end in go_pair.
+
+Definition A_close_dyn_placement_list_pair :=
+    let go_pair := A_close_dyn_placement_pair in
+    let fix go_list_pair ps :=
+        match ps with
+        | [] => True
+        | p :: ps => go_pair p /\ go_list_pair ps
+        end in go_list_pair.
+
+Ltac refold_A_close_dyn_placement :=
+    fold A_close_dyn_placement_list in *;
+    fold A_close_dyn_placement_pair in *;
+    fold A_close_dyn_placement_list_pair in *.
+
+
+
+Lemma free_list'_length : forall acc n,
+    length (free_list' acc n) = length acc + S n.
+first_induction n; intros.
+- simpl. lia.
+- simpl. rewrite IHn. simpl. lia.
+Qed.
+
+Lemma free_list_length : forall n, length (free_list n) = n.
+destruct n.
+- reflexivity.
+- eapply free_list'_length.
+Qed.
+
+Lemma close_dyn_free_length : forall drop expect,
+    length (close_dyn_free drop expect) = expect.
+intros. unfold close_dyn_free.
+rewrite skipn_length. rewrite free_list_length. lia.
+Qed.
+
+Lemma close_dyn_free_zero : forall drop, close_dyn_free drop 0 = [].
+intros.
+cut (length (close_dyn_free drop 0) = 0); [ | eapply close_dyn_free_length ].
+intro. destruct (close_dyn_free _ _); simpl in *.
+- reflexivity.
+- discriminate.
+Qed.
+
+Lemma compile_I_expr : forall AE BE ae be,
+    A_close_dyn_placement ae ->
+    compile ae = be ->
+    I_expr AE BE ae be.
+intros AE BE.
+induction ae using A.expr_rect_mut with
+    (Pl := fun aes => forall bes,
+        A_close_dyn_placement_list aes ->
+        compile_list aes = bes ->
+        Forall2 (I_expr AE BE) aes bes)
+    (Pp := fun ap => forall bp,
+        A_close_dyn_placement_pair ap ->
+        compile_pair ap = bp ->
+        I_expr AE BE (fst ap) (fst bp))
+    (Plp := fun aps => forall bps,
+        A_close_dyn_placement_list_pair aps ->
+        compile_list_pair aps = bps ->
+        Forall2 (fun ap bp => I_expr AE BE (fst ap) (fst bp)) aps bps);
+intros0 Hcdp Hcomp; simpl in Hcomp; refold_compile; try rewrite <- Hcomp;
+invc Hcdp || simpl in Hcdp; try solve [eauto | constructor; eauto].
+
+(* Call case *)
+destruct ae1; try invc Hcdp; try solve [constructor; eauto; constructor; eauto].
+destruct expect.
+- simpl. rewrite close_dyn_free_zero. eapply ICallCdzExpr. eauto.
+- constructor.
+  + constructor; eauto. lia.
+  + eauto.
+Qed.
+
+Definition var_n n :=
+    match n with
+    | 0 => B.Arg
+    | S n' => B.UpVar n'
+    end.
+
+Lemma free_list'_nth_error : forall acc n i,
+    (forall j, j < length acc ->
+        nth_error acc j = Some (var_n (S n + j))) ->
+    i < length acc + S n ->
+    nth_error (free_list' acc n) i = Some (var_n i).
+first_induction n; intros0 Hacc Hlt; simpl.
+- destruct i.
+  + simpl. reflexivity.
+  + simpl. rewrite Hacc by lia. simpl. reflexivity.
+- rewrite IHn; [ eauto | | simpl; lia ].
+  intros. simpl in *.
+  destruct j; simpl.
+  + replace (n + 0) with n by lia. reflexivity.
+  + rewrite Hacc by lia. do 2 f_equal. lia.
+Qed.
+
+Lemma free_list_nth_error : forall n i,
+    i < n ->
+    nth_error (free_list n) i = Some (var_n i).
+intros0 Hlt.
+destruct n.
+  { lia. }
+simpl. rewrite free_list'_nth_error; eauto.
+simpl. intros. lia.
+Qed.
+
+Lemma close_dyn_free_nth_error : forall drop expect i,
+    i < expect ->
+    nth_error (close_dyn_free drop expect) i = Some (var_n (drop + i)).
+intros0 Hlt.
+unfold close_dyn_free.
+rewrite skipn_nth_error. rewrite free_list_nth_error by lia.
+reflexivity.
+Qed.
+
+Lemma I_expr_value : forall AE BE a b,
+    I_expr AE BE a b ->
     A.value a ->
     B.value b.
 induction a using A.expr_ind''; intros0 II Aval; invc II; invc Aval.
 - constructor. list_magic_on (args, (bargs, tt)).
 - constructor. list_magic_on (free, (bfree, tt)).
+- constructor.
+  do 2 on (Forall _ free), fun H => eapply Forall_firstn with (n := n) in H.
+  remember (firstn n free) as afree.
+  list_magic_on (afree, (bfree, tt)).
 Qed.
 
-(*
-Lemma I_expr_value' : forall a b,
-    I_expr a b ->
+Lemma I_expr_value' : forall AE BE a b,
+    I_expr AE BE a b ->
     B.value b ->
     A.value a.
+intros ? ?.
 make_first b.
 induction b using B.expr_ind''; intros0 II Bval; invc II; invc Bval.
 - constructor. list_magic_on (args, (aargs, tt)).
 - constructor. list_magic_on (free, (afree, tt)).
-- (* not true *)
+- constructor. assumption.
+- destruct aexpect.
+  + lia.
+  + simpl.
+    fwd eapply Forall_nth_error with (P := B.value) (i := 0) as Hval; eauto.
+      { rewrite close_dyn_free_nth_error by lia. reflexivity. }
+    destruct adrop; simpl in Hval; invc Hval.
 Qed.
 
-Lemma I_expr_not_value : forall a b,
-    I_expr a b ->
+Lemma I_expr_not_value : forall AE BE a b,
+    I_expr AE BE a b ->
     ~A.value a ->
     ~B.value b.
 intros. intro. fwd eapply I_expr_value'; eauto.
 Qed.
-*)
 
-Lemma I_expr_not_value' : forall a b,
-    I_expr a b ->
+Lemma I_expr_not_value' : forall AE BE a b,
+    I_expr AE BE a b ->
     ~B.value b ->
     ~A.value a.
 intros. intro. fwd eapply I_expr_value; eauto.
@@ -525,24 +749,6 @@ intros.
 Defined.
 
 
-
-Inductive is_close_dyn_zero : A.expr -> Prop :=
-| IsCloseDynZero : forall fname drop, is_close_dyn_zero (A.CloseDyn fname drop 0).
-
-Definition is_close_dyn_zero_dec (e : A.expr) :
-    { is_close_dyn_zero e } + { ~ is_close_dyn_zero e }.
-destruct e; try solve [right; inversion 1].
-destruct expect; try solve [right; inversion 1].
-left. constructor.
-Qed.
-
-Definition is_close_dyn_zero_state s :=
-    match s with
-    | A.Run e _ _ => is_close_dyn_zero e
-    | A.Stop e => is_close_dyn_zero e
-    end.
-
-
 Definition A_matchable s := ~ is_close_dyn_zero_state s.
 
 
@@ -574,23 +780,34 @@ Qed.
 Ltac max_split :=
     repeat match goal with | [ |- _ /\ _ ] => split end.
 
-Theorem I_sim : forall AE BE a a'' b,
+
+Fixpoint metric e :=
+    match e with
+    | A.CloseDyn _ _ 0 => 1
+    | A.Call f _ => S (metric f)
+    | _ => 0
+    end.
+
+Definition state_metric s :=
+    match s with
+    | A.Run e _ _ => metric e
+    | A.Stop e => metric e
+    end.
+
+Theorem I_sim : forall AE BE a a' b,
     compile_list AE = BE ->
-    I a b ->
-    A.splus AE a a'' ->
-    A_matchable a'' ->
-    exists a' b',
-        A.splus AE a a' /\
-        A.sstar AE a' a'' /\
+    I AE BE a b ->
+    A.sstep AE a a' ->
+    exists b',
         (B.splus BE b b' \/
-         (b' = b /\ count_close_dyn_zero_state a' < count_close_dyn_zero_state a)) /\
-        I a' b'.
+         (b' = b /\ state_metric a' < state_metric a)) /\
+        I AE BE a' b'.
 
-destruct a as [ae al ak | ae]; cycle 1;
-intros0 Henv II Astep Amatch; invc_using A_splus_inv Astep; rename Astep0 into Astep;
-try solve [invc Astep].
+destruct a as [ae al ak | ae];
+intros0 Henv II Astep; [ | solve [invc Astep] ].
 
-destruct ae; inv Astep; invc II; try on (I_expr _ _), invc.
+destruct ae; inv Astep; invc II; [ try on (I_expr _ _ _ _), invc.. | ].
+
 
 - (* SArg *)
   fwd eapply nth_error_Some_ex with (n := 0) (xs := bl) as HH.
@@ -600,7 +817,7 @@ destruct ae; inv Astep; invc II; try on (I_expr _ _), invc.
   B_start HS.
   B_step HS. { eapply B.SArg. eassumption. }
 
-  do 2 eexists. max_split; eauto using A.SPlusOne.
+  eexists. split; eauto using A.SPlusOne.
   eapply H9. (* TODO - need a structtact for this *)
   + eapply Forall_nth_error; eauto.
   + eapply Forall_nth_error; eauto.
@@ -614,7 +831,7 @@ destruct ae; inv Astep; invc II; try on (I_expr _ _), invc.
   B_start HS.
   B_step HS. { eapply B.SUpVar. eassumption. }
 
-  do 2 eexists. max_split; eauto using A.SPlusOne.
+  eexists. split; eauto using A.SPlusOne.
   eapply H9. (* TODO - need a structtact for this *)
   + eapply Forall_nth_error; eauto.
   + eapply Forall_nth_error; eauto.
@@ -622,31 +839,31 @@ destruct ae; inv Astep; invc II; try on (I_expr _ _), invc.
 
 - (* SCallL *)
   destruct (is_close_dyn_zero_dec ae1).
+  all: admit.
+  (*
+
   + (* must be able to take a second step, because a' is not A_matchable. *)
-    invc Asteps.
-      { exfalso. unfold A_matchable in Amatch. simpl in Amatch. eauto. }
-      rename H into Astep'. rename H0 into Asteps.
     on (is_close_dyn_zero _), invc.
-    inv Astep'.
+    on (I_expr _ _ (A.CloseDyn _ _ _) _), invc.
+    replace (close_dyn_free drop 0) with ([] : list B.expr) by (destruct drop; auto).
 
-    do 2 eexists. max_split.
-      { eapply A.SPlusCons. eapply Astep.
-        eapply A.SPlusOne. eapply Astep'. }
-      { eapply Asteps. }
-      { right. max_split; eauto.
-        simpl. refold_count_close_dyn_zero.
-        rewrite value_count_close_dyn_zero_list by eauto using Forall_skipn.
-        lia. }
+    eexists. split; eauto.
+    eapply ICallCloseDyn; eauto.
+    simpl in *. B.refold_num_locals. lia.
 
-    constructor; eauto.
-    constructor; eauto.
-    on (I_expr _ bf), invc.
-    constructor.
-    admit. (* need to allow varying numbers of upvars in IClose *)
+  + B_start HS.
+    B_step HS. { eapply B.SCallL. admit. (* broken *) }
+    eexists. split. left. eauto.
+    admit.
+  *)
 
-  + admit.
+Admitted.
 
-- (* SCallR *) admit.
+(* number/order of subgoals may have changed...
+
+- (* SCallR *)
+  B_start HS.
+  B_step HS. { eapply B.SCallR.
 
 - (* SMakeCall *) admit.
 
@@ -665,3 +882,4 @@ destruct ae; inv Astep; invc II; try on (I_expr _ _), invc.
 - (* SCloseDyn *) admit.
 
 Admitted.
+*)
