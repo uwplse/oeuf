9334dc3aa01949040584beee9c274a09134c79f4
Eric A Mullen <emullen@cs.washington.edu>
2016-10-05 17:29:55 -0700

    top level theorems yay

--- src/OeufProof.v
+++ src/OeufProof.v
@@ -11,6 +11,12 @@ Require Import compcert.common.Errors.
 Require Import compcert.common.Events.
 Require Import compcert.common.Globalenvs.
 
+Require Import StructTact.StructTactics.
+Require Import StructTact.Util.
+
+Require Import EricTact.
+
+
 Inductive simple_star {A : Type} (step : A -> A -> Prop) : A -> A -> Prop :=
 | star_nil :
     forall st,
@@ -21,24 +27,86 @@ Inductive simple_star {A : Type} (step : A -> A -> Prop) : A -> A -> Prop :=
       simple_star step st2 st3 ->
       simple_star step st1 st3.
 
-(* Dummy definition, fill in later *)
+Section Simulation.
+
+  Variable prog : compilation_unit.
+  Variable tprog : Asm.program.
+  Hypothesis TRANSF : transf_to_asm prog = OK tprog.
+  Definition ge := Genv.globalenv tprog.
+
+(* Dummy top level definition, fill in later *)
 Inductive correspond {tys ty} (exp : SourceLang.expr tys ty) (st : Asm.state).
-      
-Theorem Oeuf_step_sim :
-  forall cu asmprog,
-    transf_to_asm cu = OK asmprog ->
-    forall tys ty expr,
-      CompilationUnit.initial_state cu tys ty expr ->
-      forall st,
-        Asm.initial_state asmprog st ->
-        correspond expr st ->
-        forall expr',
-          simple_star SourceLang.step expr expr' ->
-          exists st',
-            star Asm.step (Genv.globalenv asmprog) st E0 st' /\
-            correspond expr' st'.
+
+Inductive match_states {tys ty} (exp : SourceLang.expr tys ty) (st : Asm.state).
+
+  
+Lemma match_correspond :
+  forall {tys ty} (exp : SourceLang.expr tys ty) st,
+    match_states exp st ->
+    correspond exp st.
 Proof.
 Admitted.
+
+Lemma correspond_initial_match :
+  forall {tys ty} expr,
+      CompilationUnit.initial_state prog tys ty expr ->
+      forall st,
+        Asm.initial_state tprog st ->
+        correspond expr st ->
+        match_states expr st.
+Proof.
+Admitted.
+
+Lemma step_sim :
+  forall {tys ty} (exp exp' : SourceLang.expr tys ty),
+    SourceLang.step exp exp' ->
+    forall st,
+      match_states exp st ->
+      exists st',
+        plus Asm.step ge st E0 st' /\
+        match_states exp' st'.
+Proof.
+Admitted.
+
+Lemma star_step_sim :
+  forall {tys ty} (exp exp' : SourceLang.expr tys ty),
+    simple_star SourceLang.step exp exp' ->
+    forall st,
+      match_states exp st ->
+      exists st',
+        star Asm.step ge st E0 st' /\
+        match_states exp' st'.
+Proof.
+  induction 1; intros.
+  solve [eexists; split; eauto; eapply star_refl].
+  eapply step_sim in H; eauto.
+  break_exists. break_and.
+  app plus_star plus.
+  destruct (IHsimple_star x); eauto.
+  break_and. eexists; split;
+               try eapply H5.
+  eapply star_trans; eauto.
+Qed.
+  
+
+Theorem Oeuf_sim :
+  forall tys ty expr expr',
+    simple_star SourceLang.step expr expr' ->
+    forall st,
+      CompilationUnit.initial_state prog tys ty expr ->
+      Asm.initial_state tprog st ->
+      correspond expr st ->
+      exists st',
+        star Asm.step ge st E0 st' /\
+        correspond expr' st'.
+Proof.
+  intros.
+  eapply correspond_initial_match in H2; eauto.
+  eapply star_step_sim in H; eauto.
+  break_exists. break_and.
+  eexists; split; eauto.
+  eapply match_correspond; eauto.
+Qed.
 
 Theorem Oeuf_correspond_exists :
   forall cu asmprog,
@@ -50,4 +118,5 @@ Theorem Oeuf_correspond_exists :
         correspond expr st.
 Proof.
 Admitted.
-  
+
+End Simulation.
