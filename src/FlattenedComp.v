89505cce0aa5e8aeb68ccc9f0579a508373de124
Stuart Pernsteiner <spernste@cs.washington.edu>
2016-10-18 10:54:07 -0700

    better alignment between SelfNumbered and Flattened

--- src/FlattenedComp.v
+++ src/FlattenedComp.v
@@ -1,85 +1,154 @@
 Require Import Common Monads.
 Require Import Metadata.
 Require SelfNumbered Flattened.
+Require Import StructTact.Assoc.
+Require Import Forall3.
 
 Module A := SelfNumbered.
 Module B := Flattened.
 
-Section compile.
+Set Default Timeout 10.
 
-  Open Scope state_monad.
 
-  Definition compiler_monad A := state nat A.
 
-  Definition fresh : compiler_monad nat :=
-    get >>= fun n => put (S n) >>= fun _ => ret_state n.
-
-  Fixpoint sequence (l : list B.stmt) : B.stmt :=
-    match l with
-    | [] => B.Skip
-    | s :: l' => B.Seq s (sequence l')
+Fixpoint unzip {A B} (xs : list (A * B)) : list A * list B :=
+    match xs with
+    | [] => ([], [])
+    | (a, b) :: xs =>
+            let (as_, bs) := unzip xs in
+            (a :: as_, b :: bs)
     end.
 
-  Definition compile' (e : A.expr) : compiler_monad (B.stmt * B.expr) :=
-    let fix go (e : A.expr) : compiler_monad (B.stmt * B.expr) :=
-        let fix go_list (l : list A.expr) : compiler_monad (list (B.stmt * B.expr)) :=
-            match l with
-            | [] => ret_state []
-            | e :: l' => cons <$> go e <*> go_list l'
-            end
-        in
+Fixpoint zip {A B} (xs : list A) (ys : list B) : list (A * B) :=
+    match xs, ys with
+    | [], [] => []
+    | [], _ => []
+    | _, [] => []
+    | x :: xs, y :: ys => (x, y) :: zip xs ys
+    end.
+
+
+
+Definition compile : A.expr -> B.stmt * B.expr :=
+    let fix go e :=
+        let fix go_list es : list (B.stmt * B.expr) :=
+            match es with
+            | [] => []
+            | e :: es => go e :: go_list es
+            end in
         match e with
-        | A.Arg => ret_state (B.Skip, B.Arg)
-        | A.Self => ret_state (B.Skip, B.Self)
-        | A.Deref e' n =>
-          go e' >>= fun p =>
-          let (s', e') := p in
-          ret_state (s', B.Deref e' n)
-        | A.Call _ f a =>
-          go f >>= fun pf =>
-          let (sf, f) := pf in
-          go a >>= fun pa =>
-          let (sa, a) := pa in
-          fresh >>= fun dst =>
-          ret_state (B.Seq sf (B.Seq sa (B.Call dst f a)), B.Temp dst)
-        | A.Constr _ tag args =>
-          go_list args >>= fun psargs =>
-          let (sargs, args) := split psargs in
-          fresh >>= fun dst =>
-          ret_state (B.Seq (sequence sargs) (B.MakeConstr dst tag args), B.Temp dst)
-        | A.Switch cases =>
-          go_list cases >>= fun pscases =>
-          (* target is implicitly A.Skip *)
-          let (starget, target) := (B.Skip, B.Arg) in
-          fresh >>= fun dst =>
-          ret_state (B.Seq starget (B.Switch (map (fun p => let (s, e) := p : _ * _ in B.Seq s (B.Assign dst e)) pscases) target), B.Temp dst)
-        | A.Close _ fn args =>
-          go_list args >>= fun psargs =>
-          let (sargs, args) := split psargs in
-          fresh >>= fun dst =>
-          ret_state (B.Seq (sequence sargs) (B.MakeClose dst fn args), B.Temp dst)
-        | A.Copy _ e => go e
-        end
-    in go e.
+        | A.Arg => (B.Skip, B.Arg)
+        | A.Self => (B.Skip, B.Self)
+        | A.Deref e off =>
+                let '(s, e') := go e in
+                (s, B.Deref e' off)
+        | A.Call dst f a =>
+                let '(fs, f') := go f in
+                let '(as_, a') := go a in
+                (B.Seq (B.Seq fs as_) (B.Call dst f' a'), B.Temp dst)
+        | A.Constr dst tag args =>
+                let '(ss, args') := unzip (go_list args) in
+                (fold_right B.Seq (B.MakeConstr dst tag args') ss, B.Temp dst)
+        | A.Switch dst cases =>
+                (B.Switch dst (go_list cases) B.Arg, B.Temp dst)
+        | A.Close dst fname free =>
+                let '(ss, free') := unzip (go_list free) in
+                (fold_right B.Seq (B.MakeClose dst fname free') ss, B.Temp dst)
+        end in go.
 
-  Definition compile (e : A.expr) : B.stmt * B.expr := fst (compile' e 0).
+Definition compile_list :=
+    let go := compile in
+    let fix go_list es : list (B.stmt * B.expr) :=
+        match es with
+        | [] => []
+        | e :: es => go e :: go_list es
+        end in go_list.
 
-  Fixpoint compile_env' (l : list A.expr) : compiler_monad (list (B.stmt * B.expr)) :=
-    match l with
-    | [] => ret_state []
-    | e :: l' => cons <$> compile' e <*> compile_env' l'
-    end.
+Ltac refold_compile := fold compile_list in *.
 
-  Definition compile_env (l : list A.expr) : list (B.stmt * B.expr) := fst (compile_env' l 0).
 
-  Definition compile_cu (cu : list A.expr * list metadata) : list (B.stmt * B.expr) * list metadata :=
-    let '(exprs, metas) := cu in
-    let exprs' := compile_env exprs in
-    (exprs', metas).
 
-End compile.
+Inductive I_value : A.expr -> B.value -> Prop :=
+| IvConstr : forall num tag aargs bargs,
+        Forall2 I_value aargs bargs ->
+        I_value (A.Constr num tag aargs) (B.Constr tag bargs)
+| IvClose : forall num tag afree bfree,
+        Forall2 I_value afree bfree ->
+        I_value (A.Close num tag afree) (B.Close tag bfree)
+.
 
-(* Eval compute in compile_prog Switched.add_prog2. *)
+Definition assoc' {V} := assoc (V := V) eq_nat_dec.
+
+Inductive I_expr (l : list (nat * B.value)) : A.expr -> B.stmt -> B.expr -> Prop :=
+| IValue : forall av bv i,
+        assoc' l i = Some bv ->
+        I_value av bv ->
+        I_expr l av B.Skip (B.Temp i)
+| IArg : I_expr l A.Arg B.Skip B.Arg
+| ISelf : I_expr l A.Self B.Skip B.Self
+| IDeref : forall ae bs be off,
+        I_expr l ae bs be ->
+        I_expr l (A.Deref ae off) bs (B.Deref be off)
+| ICall : forall dst af aa bfs bfe bas bae,
+        I_expr l af bfs bfe ->
+        I_expr l aa bas bae ->
+        I_expr l (A.Call dst af aa) (B.Seq (B.Seq bfs bas) (B.Call dst bfe bae)) (B.Temp dst)
+| IConstr : forall dst tag aargs bstmts bargs,
+        Forall3 (I_expr l) aargs bstmts bargs ->
+        I_expr l (A.Constr dst tag aargs)
+                 (fold_right B.Seq (B.MakeConstr dst tag bargs) bstmts)
+                 (B.Temp dst)
+| ISwitch : forall dst acases bcases bstmts bexprs,
+        (bstmts, bexprs) = unzip bcases ->
+        Forall3 (I_expr l) acases bstmts bexprs ->
+        I_expr l (A.Switch dst acases)
+                 (B.Switch dst bcases B.Arg)
+                 (B.Temp dst)
+| IClose : forall dst fname afree bstmts bfree,
+        Forall3 (I_expr l) afree bstmts bfree ->
+        I_expr l (A.Close dst fname afree)
+                 (fold_right B.Seq (B.MakeClose dst fname bfree) bstmts)
+                 (B.Temp dst)
+.
+
+
+
+Theorem compile_I_expr : forall l ae bs be,
+    compile ae = (bs, be) ->
+    I_expr l ae bs be.
+intros l.
+induction ae using A.expr_rect_mut with
+    (Pl := fun aes => forall bps,
+        compile_list aes = bps ->
+        let '(bss, bes) := unzip bps in
+        Forall3 (I_expr l) aes bss bes);
+intros0 Hcomp; simpl in Hcomp; repeat break_match; refold_compile; try invc Hcomp;
+try solve [econstructor; eauto].
+
+- (* MakeConstr *)
+  specialize (IHae ?? ***). break_match.
+  inject_pair.  constructor; eauto.
+
+- (* Switch *)
+  specialize (IHae ?? ***). break_match.
+  econstructor; eauto.
+
+- (* MakeClose *)
+  specialize (IHae ?? ***). break_match.
+  inject_pair.  constructor; eauto.
+
+- (* nil *)
+  subst. simpl in *. inject_pair. constructor.
+
+- (* cons *)
+  destruct (compile ae) as [bs be].
+  specialize (IHae ?? ?? ***).
+  specialize (IHae0 ?? ***). break_match.
+  destruct bps; try discriminate.
+  simpl in *. do 2 break_match. inject_pair.
+  constructor; congruence.
+Qed.
+
 
 
 (*
