ec0740859faa2969fb937ce3dae3a10c814ef2ac
Stuart Pernsteiner <spernste@cs.washington.edu>
2016-10-21 15:20:57 -0700

    remove nops

---
+++ src/StackFlatter2.v
@@ -0,0 +1,202 @@
+Require Import Common.
+Require Import Psatz.
+
+Require Import Utopia.
+Require Import Monads.
+
+Require Export HigherValue.
+
+Inductive insn :=
+| Arg
+| Self
+| Deref (off : nat)
+| Call
+| MkConstr (tag : nat) (nargs : nat)
+| Switch (cases : list (list insn))
+| MkClose (f : function_name) (nfree : nat)
+.
+
+Definition env := list (list insn).
+
+
+(* Continuation-based step relation *)
+
+Record frame := Frame {
+    arg : value;
+    self : value;
+    stack : list value
+}.
+
+Definition push f v :=
+    Frame (arg f) (self f) (v :: stack f).
+
+Definition pop f n :=
+    Frame (arg f) (self f) (skipn n (stack f)).
+
+Definition pop_push f n v :=
+    push (pop f n) v.
+
+Definition top f :=
+    match stack f with
+    | [] => Constr 0 []
+    | v :: _ => v
+    end.
+
+
+
+Inductive cont :=
+| Kret (code : list insn) (f : frame) (k : cont)
+| Kstop.
+
+Inductive state :=
+| Run (i : list insn) (f : frame) (k : cont)
+| Stop (v : value).
+
+Inductive sstep (E : env) : state -> state -> Prop :=
+| SArg : forall is f k,
+        sstep E (Run (Arg :: is) f k)
+                (Run is (push f (arg f)) k)
+| SSelf : forall is f k,
+        sstep E (Run (Self :: is) f k)
+                (Run is (push f (self f)) k)
+
+| SDerefinateConstr : forall off is f k  tag args v,
+        length (stack f) >= 1 ->
+        top f = Constr tag args ->
+        nth_error args off = Some v ->
+        sstep E (Run (Deref off :: is) f k)
+                (Run is (pop_push f 1 v) k)
+| SDerefinateClose : forall off is f k  fname free v,
+        length (stack f) >= 1 ->
+        top f = Close fname free ->
+        nth_error free off = Some v ->
+        sstep E (Run (Deref off :: is) f k)
+                (Run is (pop_push f 1 v) k)
+
+| SConstrDone : forall tag nargs is f k,
+        length (stack f) >= nargs ->
+        sstep E (Run (MkConstr tag nargs :: is) f k)
+                (Run is (pop_push f nargs (Constr tag (rev (firstn nargs (stack f))))) k)
+| SCloseDone : forall fname nfree is f k,
+        length (stack f) >= nfree ->
+        sstep E (Run (MkClose fname nfree :: is) f k)
+                (Run is (pop_push f nfree (Close fname (rev (firstn nfree (stack f))))) k)
+
+| SMakeCall : forall is f k  fname free body,
+        length (stack f) >= 2 ->
+        nth_error (stack f) 1 = Some (Close fname free) ->
+        nth_error E fname = Some body ->
+        sstep E (Run (Call :: is) f k)
+                (Run body (Frame (top f) (Close fname free) [])
+                    (Kret is (pop f 2) k))
+
+(* NB: `Switch` still has an implicit target of `Arg` *)
+| SSwitchinate : forall cases is f k  tag args case,
+        arg f = Constr tag args ->
+        nth_error cases tag = Some case ->
+        sstep E (Run (Switch cases :: is) f k)
+                (Run (case ++ is) f k)
+
+| SContRet : forall code f f' k,
+        sstep E (Run [] f (Kret code f' k))
+                (Run code (push f' (top f)) k)
+| SContStop : forall f,
+        sstep E (Run [] f Kstop)
+                (Stop (top f))
+.
+
+Inductive sstar (E : env) : state -> state -> Prop :=
+| SStarNil : forall e, sstar E e e
+| SStarCons : forall e e' e'',
+        sstep E e e' ->
+        sstar E e' e'' ->
+        sstar E e e''.
+
+Inductive splus (E : env) : state -> state -> Prop :=
+| SPlusOne : forall s s',
+        sstep E s s' ->
+        splus E s s'
+| SPlusCons : forall s s' s'',
+        sstep E s s' ->
+        splus E s' s'' ->
+        splus E s s''.
+
+
+
+(*
+ * Mutual recursion/induction schemes for expr
+ *)
+
+Definition insn_rect_mut
+        (P : insn -> Type)
+        (Pl : list insn -> Type)
+        (Pll : list (list insn) -> Type)
+    (HArg :     P Arg)
+    (HSelf :    P Self)
+    (HDeref :   forall off, P (Deref off))
+    (HCall :    P Call)
+    (HConstr :  forall tag nargs, P (MkConstr tag nargs))
+    (HSwitch :  forall cases, Pll cases -> P (Switch cases))
+    (HClose :   forall fname nfree, P (MkClose fname nfree))
+    (Hnil :     Pl [])
+    (Hcons :    forall i is, P i -> Pl is -> Pl (i :: is))
+    (Hnil2 :    Pll [])
+    (Hcons2 :   forall is iss, Pl is -> Pll iss -> Pll (is :: iss))
+    (i : insn) : P i :=
+    let fix go i :=
+        let fix go_list is :=
+            match is as is_ return Pl is_ with
+            | [] => Hnil
+            | i :: is => Hcons i is (go i) (go_list is)
+            end in
+        let fix go_list_list iss :=
+            match iss as iss_ return Pll iss_ with
+            | [] => Hnil2
+            | is :: iss => Hcons2 is iss (go_list is) (go_list_list iss)
+            end in
+        match i as i_ return P i_ with
+        | Arg => HArg
+        | Self => HSelf
+        | Deref off => HDeref off
+        | Call => HCall
+        | MkConstr tag nargs => HConstr tag nargs
+        | Switch cases => HSwitch cases (go_list_list cases)
+        | MkClose fname nfree => HClose fname nfree
+        end in go i.
+
+(* Useful wrapper for `expr_rect_mut with (Pl := Forall P)` *)
+Definition insn_ind' (P : insn -> Prop)
+    (HArg :     P Arg)
+    (HSelf :    P Self)
+    (HDeref :   forall off, P (Deref off))
+    (HCall :    P Call)
+    (HConstr :  forall tag nargs, P (MkConstr tag nargs))
+    (HSwitch :  forall cases, Forall (Forall P) cases -> P (Switch cases))
+    (HClose :   forall fname nfree, P (MkClose fname nfree))
+    (i : insn) : P i :=
+    ltac:(refine (@insn_rect_mut P (Forall P) (Forall (Forall P))
+        HArg HSelf HDeref HCall HConstr HSwitch HClose _ _ _ _ i); eauto).
+
+Definition insn_list_rect_mut
+        (P : insn -> Type)
+        (Pl : list insn -> Type)
+        (Pll : list (list insn) -> Type)
+    (HArg :     P Arg)
+    (HSelf :    P Self)
+    (HDeref :   forall off, P (Deref off))
+    (HCall :    P Call)
+    (HConstr :  forall tag nargs, P (MkConstr tag nargs))
+    (HSwitch :  forall cases, Pll cases -> P (Switch cases))
+    (HClose :   forall fname nfree, P (MkClose fname nfree))
+    (Hnil :     Pl [])
+    (Hcons :    forall i is, P i -> Pl is -> Pl (i :: is))
+    (Hnil2 :    Pll [])
+    (Hcons2 :   forall is iss, Pl is -> Pll iss -> Pll (is :: iss))
+    (is : list insn) : Pl is :=
+    let go := insn_rect_mut P Pl Pll
+            HArg HSelf HDeref HCall HConstr HSwitch HClose Hnil Hcons Hnil2 Hcons2 in
+    let fix go_list is :=
+        match is as is_ return Pl is_ with
+        | [] => Hnil
+        | i :: is => Hcons i is (go i) (go_list is)
+        end in go_list is.
---
+++ src/StackFlatterComp2.v
@@ -0,0 +1,267 @@
+Require Import Common Monads.
+Require Import Metadata.
+Require String.
+Require StackFlatter StackFlatter2.
+Require Import ListLemmas.
+Require Import HigherValue.
+
+Require Import Psatz.
+
+Module A := StackFlatter.
+Module B := StackFlatter2.
+
+
+Definition compile_one : A.insn -> list B.insn :=
+    let fix go e :=
+        let fix go_block (es : list A.insn) : list B.insn :=
+            match es with
+            | [] => []
+            | e :: es => go e ++ go_block es
+            end in
+        let fix go_block_list (es : list (list A.insn)) : list (list B.insn) :=
+            match es with
+            | [] => []
+            | e :: es => go_block e :: go_block_list es
+            end in
+        match e with
+        | A.Nop => []
+        | A.Arg => [B.Arg]
+        | A.Self => [B.Self]
+        | A.Deref off => [B.Deref off]
+        | A.Call => [B.Call]
+        | A.MkConstr tag nargs => [B.MkConstr tag nargs]
+        | A.Switch cases => [B.Switch (go_block_list cases)]
+        | A.MkClose fname nfree => [B.MkClose fname nfree]
+        end in go.
+
+Definition compile_one_block :=
+    let go := compile_one in
+    let fix go_block es :=
+        match es with
+        | [] => []
+        | e :: es => go e ++ go_block es
+        end in go_block.
+
+Definition compile_one_block_list :=
+    let go_block := compile_one_block in
+    let fix go_block_list es :=
+        match es with
+        | [] => []
+        | e :: es => go_block e :: go_block_list es
+        end in go_block_list.
+
+Definition compile : list A.insn -> list B.insn := compile_one_block.
+
+Ltac refold_compile :=
+    fold compile_one_block in *;
+    fold compile_one_block_list in *.
+
+
+
+Inductive I_insns : list A.insn -> list B.insn -> Prop :=
+| INil : I_insns [] []
+| INop : forall acode bcode,
+        I_insns acode bcode ->
+        I_insns (A.Nop :: acode) (bcode)
+| IArg : forall acode bcode,
+        I_insns acode bcode ->
+        I_insns (A.Arg :: acode) (B.Arg :: bcode)
+| ISelf : forall acode bcode,
+        I_insns acode bcode ->
+        I_insns (A.Self :: acode) (B.Self :: bcode)
+| IDeref : forall off acode bcode,
+        I_insns acode bcode ->
+        I_insns (A.Deref off :: acode) (B.Deref off :: bcode)
+| ICall : forall acode bcode,
+        I_insns acode bcode ->
+        I_insns (A.Call :: acode) (B.Call :: bcode)
+| IMkConstr : forall tag nargs acode bcode,
+        I_insns acode bcode ->
+        I_insns (A.MkConstr tag nargs :: acode) (B.MkConstr tag nargs :: bcode)
+| ISwitch : forall acases bcases acode bcode,
+        Forall2 I_insns acases bcases ->
+        I_insns acode bcode ->
+        I_insns (A.Switch acases :: acode) (B.Switch bcases :: bcode)
+| IMkClose : forall tag nargs acode bcode,
+        I_insns acode bcode ->
+        I_insns (A.MkClose tag nargs :: acode) (B.MkClose tag nargs :: bcode)
+.
+
+Inductive I_frame : A.frame -> B.frame -> Prop :=
+| IFrame : forall arg self stk,
+        I_frame (A.Frame arg self stk) (B.Frame arg self stk).
+Hint Constructors I_frame.
+
+Inductive I_cont : A.cont -> B.cont -> Prop :=
+| IkRet : forall acode af ak bcode bf bk,
+        I_insns acode bcode ->
+        I_frame af bf ->
+        I_cont ak bk ->
+        I_cont (A.Kret acode af ak) (B.Kret bcode bf bk)
+| IkStop : I_cont A.Kstop B.Kstop.
+
+Inductive I : A.state -> B.state -> Prop :=
+| IRun : forall acode af ak  bcode bf bk,
+        I_insns acode bcode ->
+        I_frame af bf ->
+        I_cont ak bk ->
+        I (A.Run acode af ak)
+          (B.Run bcode bf bk)
+
+| IStop : forall v,
+        I (A.Stop v) (B.Stop v).
+
+
+
+Lemma I_insns_inv : forall acode bcode
+        (P : _ -> _ -> Prop),
+    (acode = [] ->
+        bcode = [] ->
+        P acode bcode) ->
+    (forall ainsn acode'' bcode' bcode'',
+        acode = ainsn :: acode'' ->
+        bcode = bcode' ++ bcode'' ->
+        I_insns [ainsn] bcode' ->
+        I_insns acode'' bcode'' ->
+        P acode bcode) ->
+    I_insns acode bcode -> P acode bcode.
+intros0 HPnil HPcons II.
+invc II; try solve [
+    eapply HPnil + eapply HPcons;
+    try solve [eauto | constructor; eauto using INil];
+    reflexivity
+].
+Qed.
+
+Lemma cons_I_insns : forall ainsn acode' bcode bcode',
+    I_insns [ainsn] bcode ->
+    I_insns acode' bcode' ->
+    I_insns (ainsn :: acode') (bcode ++ bcode').
+intros0 II II'; destruct ainsn;
+try solve [invc II; on >I_insns, invc; constructor; auto].
+Qed.
+
+Lemma app_I_insns : forall acode acode' bcode bcode',
+    I_insns acode bcode ->
+    I_insns acode' bcode' ->
+    I_insns (acode ++ acode') (bcode ++ bcode').
+first_induction acode; intros0 II II'; inv_using I_insns_inv II; try discriminate.
+- simpl. assumption.
+- on (_ :: _ = _ :: _), invc.
+  rewrite <- app_assoc.
+  rewrite <- app_comm_cons.
+  eapply cons_I_insns; eauto.
+Qed.
+
+Theorem compile_I_expr : forall a b,
+    compile a = b ->
+    I_insns a b.
+induction a using A.insn_list_rect_mut with
+    (P := fun a => forall b,
+        compile_one a = b ->
+        I_insns [a] b)
+    (Pl := fun a => forall b,
+        compile_one_block a = b ->
+        I_insns a b)
+    (Pll := fun a => forall b,
+        compile_one_block_list a = b ->
+        Forall2 I_insns a b);
+intros0 Hcomp; simpl in Hcomp; refold_compile; try (rewrite <- Hcomp; clear Hcomp);
+try solve [econstructor; eauto using INil].
+- eapply cons_I_insns; eauto.
+Qed.
+
+
+
+Ltac i_ctor := intros; constructor; eauto.
+Ltac i_lem H := intros; eapply H; eauto.
+
+Ltac stk_simpl := unfold A.pop_push, A.push, B.pop_push, B.push in *; simpl in *.
+
+Ltac stk_simpl' := compute [
+    A.pop A.push A.pop_push  A.arg A.self A.stack
+    B.pop B.push B.pop_push  B.arg B.self B.stack
+    ] in *.
+
+Fixpoint code_metric is :=
+    match is with
+    | A.Nop :: is => S (code_metric is)
+    | _ => 0
+    end.
+
+Definition metric s :=
+    match s with
+    | A.Run code _ _ => code_metric code
+    | A.Stop _ => 0
+    end.
+
+Theorem I_sim : forall AE BE a a' b,
+    Forall2 I_insns AE BE ->
+    I a b ->
+    A.sstep AE a a' ->
+    exists b',
+        (B.sstep BE b b' \/ (b' = b /\ metric a' < metric a)) /\
+        I a' b'.
+
+destruct a as [ae al ak | ae];
+intros0 Henv II Astep; [ | solve [invc Astep] ].
+
+inv Astep; inv II;
+try on (Forall2 I_insn _ _), invc;
+try on (Forall2 _ [] _), invc;
+try on (I_insn _ _), invc;
+try on >I_frame, inv;
+try (on >I_insns, inv; []);
+simpl in *; try subst.
+
+
+- (* Nop *)
+  eexists. split. right. split. reflexivity. { lia. }
+  i_ctor.
+
+- (* Arg *)
+  eexists. split. left. eapply B.SArg.
+  i_ctor. i_ctor.
+
+- (* Self *)
+  eexists. split. left. eapply B.SSelf.
+  i_ctor. i_ctor.
+
+- (* DerefinateConstr *)
+  eexists. split. left. eapply B.SDerefinateConstr; eauto using eq_refl.
+  i_ctor. i_ctor.
+
+- (* DerefinateClose *)
+  eexists. split. left. eapply B.SDerefinateClose; eauto using eq_refl.
+  i_ctor. i_ctor.
+
+- (* MkConstr *)
+  eexists. split. left. eapply B.SConstrDone; eauto using eq_refl.
+  i_ctor. i_ctor.
+
+- (* MkClose *)
+  eexists. split. left. eapply B.SCloseDone; eauto using eq_refl.
+  i_ctor. i_ctor.
+
+- (* MakeCall *)
+  fwd eapply Forall2_nth_error_ex as HH; eauto.  destruct HH as (bbody & ? & ?).
+
+  eexists. split. left. eapply B.SMakeCall; eauto using eq_refl.
+  i_ctor. i_ctor. i_ctor.
+
+- (* Switchinate *)
+  fwd eapply Forall2_nth_error_ex as HH; eauto.  destruct HH as (bcase & ? & ?).
+
+  eexists. split. left. eapply B.SSwitchinate; eauto using eq_refl.
+  i_ctor. eapply app_I_insns; eauto.
+
+- (* ContRet *)
+  on >I_cont, inv.
+  eexists. split. left. eapply B.SContRet; eauto using eq_refl.
+  i_ctor. simpl. on (I_frame f' bf), invc. i_ctor.
+
+- (* ContStop *)
+  on >I_cont, invc.
+  eexists. split. left. eapply B.SContStop; eauto using eq_refl.
+  i_ctor.
+Qed.
