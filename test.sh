e5a6ec9195e57a64c3ad7d9e5893fe68e1f3b23d
Eric Mullen <emullen@warfa.cs.washington.edu>
2016-10-26 16:25:32 -0700

    too much hidden from logs

--- test.sh
+++ test.sh
@@ -14,7 +14,7 @@ for f in test/*.v ; do
 # so those rules that are necessary are repeated here
     $COQC -Q src "" "$f" >/dev/null 2>&1 || { echo "coqc failed on $f"; exit 1; }
     TESTNAME=$(basename "$f" ".v" | tr '[:upper:]' '[:lower:]')
-    ./occ.sh "$TESTNAME" >/dev/null 2>&1 || { echo "occ.sh failed on $TESTNAME"; exit 1; }
+    ./occ.sh "$TESTNAME" || { echo "occ.sh failed on $TESTNAME"; exit 1; }
     ./"$TESTNAME" > "test/${TESTNAME}.actual" || { echo "./a.out failed on $TESTNAME"; exit 1; }
     if ! diff -q "test/${TESTNAME}.expected" "test/${TESTNAME}.actual"
     then echo "Test $TESTNAME failed!"
