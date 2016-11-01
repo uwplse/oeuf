7c768c6a712a8076a087db68f4fdeb2b9ea33acf
Eric A Mullen <emullen@cs.washington.edu>
2016-11-01 15:30:52 -0700

    work on top level theorems

--- src/OeufProof.v
+++ src/OeufProof.v
@@ -6,6 +6,7 @@ Require Import StepLib.
 Require Import MixSemantics.
 
 Require Import SourceLang.
+Require Import HighValues.
 
 Require UntypedComp.
 Require LiftedComp.
@@ -40,19 +41,13 @@ Require Import StructTact.Util.
 
 Require Import EricTact.
 
-
-(* All of the things we need from the top level: *)
-(* 1. forward step simulation: step in SourceLang corresponds to steps in Asm, assuming matching states *)
-(* 2. match states establishment: either start from an exp we compiled, or take two values and compose them, and we get establish match_states between top and Asm states *)
-(* 3. backwards simulation: use the fact that Asm is deterministic to show that the target program can't do anything else but what we want *)
-(* 4. cotermination: show that anything that a value matches to can't step *)
-
 Section Simulation.
 
   Variable prog : compilation_unit.
   Variable tprog : Asm.program.
   Hypothesis TRANSF : transf_to_asm prog = OK tprog.
 
+  (* In this theorem we grab all of the things we need from all of the passes *)
   Theorem Oeuf_forward_simulation :
     forall ty,
       mix_forward_simulation (@CompilationUnit.source_semantics ty prog) (Asm.semantics tprog).
@@ -151,6 +146,7 @@ Section Simulation.
     eapply Dmajortodflatmajor.fsim; try eassumption.
 
     admit. admit.
+
     (* Dflatmajor to Cmajor *)
     eapply compose_forward_simulation.
     eapply Dflatmajortocmajor.fsim; try eassumption.
@@ -168,23 +164,98 @@ Section Simulation.
     
   Admitted.
 
+
+
+
+  
+  (* TODO *)
+  (* Top level match states: we will write by hand, and make an easier definition to read *)
+  Inductive match_states {ty} (expr : SourceLang.expr nil ty) (st : Asm.state) : Prop :=.
+
+  Lemma match_states_equiv :
+    forall {ty} expr st,
+      match_states expr st <-> (exists i, Oeuf_forward_simulation ty i expr st).
+  Proof.
+  Admitted.
+
   Theorem Oeuf_step_sim :
     forall ty expr expr',
       Semantics.star _ _ (fun _ => SourceLang.step) tt expr expr' ->
-      forall st i,
+      forall st,
         CompilationUnit.initial_state prog nil ty expr ->
         Asm.initial_state tprog st ->
-        Oeuf_forward_simulation ty i expr st ->
-        exists st' i',
+        match_states expr st ->
+        exists st',
           star Asm.step (Genv.globalenv tprog) st E0 st' /\
-          Oeuf_forward_simulation ty i' expr' st'.
-    Proof.
-      intros.
-      edestruct MixSemantics.simulation_star; eauto.
-      simpl. eassumption.
-      break_exists. break_and.
-      repeat eexists; eauto.
-    Qed.
+          match_states expr' st'.
+  Proof.
+    intros.
+    rewrite match_states_equiv in H2. break_exists.
+    edestruct MixSemantics.simulation_star; eauto.
+    simpl. eassumption.
+    break_exists. break_and.
+    repeat eexists; eauto.
+    eapply match_states_equiv; eauto.
+  Qed.
 
+  Theorem final_states_match :
+    forall {ty} (expr : SourceLang.expr nil ty) st,
+      SourceLang.value expr ->
+      match_states expr st ->
+      exists r,
+        Asm.final_state st r. (* /\ value_inject expr r m*)
+  Proof.
+    (* TODO: make r not an int, but a pointer. *)
+  Admitted.
+
+  Theorem final_state_nostep :
+    forall st r,
+      Asm.final_state st r ->
+      nostep Asm.step (Genv.globalenv tprog) st.
+  Proof.
+    intros.
+    remember (Asm.semantics_determinate tprog) as H2.
+    clear HeqH2.
+    inv H2. eauto.
+  Qed.
+
+  Definition match_initial_states_field {ty} := (MixSemantics.fsim_match_initial_states (source_semantics prog) (Asm.semantics tprog) (Oeuf_forward_simulation ty)).
+
+  (* Here's how we show that we establish matching for initial symbols in our program *)
+  (* i.e. what we call to build closures *)
+  Theorem match_initial_states :
+    forall ty expr,
+      CompilationUnit.initial_state prog nil ty expr ->
+      exists st,
+        Asm.initial_state tprog st /\ match_states expr st.
+  Proof.
+    intros.
+    eapply match_initial_states_field in H; eauto.
+    break_exists. break_and.
+    assert (exists x, (Oeuf_forward_simulation ty) x expr x0) by eauto.
+    rewrite <- match_states_equiv in H1.
+    eauto.
+  Qed.
+
+
+  Definition compose_states (st1 st2 : Asm.state) : Asm.state :=
+    (* TODO write this function  *)
+    st1.
+  
+  (* Here we establish matching for two previously matching values *)
+  (* this depends entirely on our definition of match_states being sane *)
+  Theorem match_compose_states :
+    forall {ty1 ty2} (f : SourceLang.expr nil (Arrow ty1 ty2)) (v : SourceLang.expr nil ty1) st1 st2,
+      SourceLang.value f ->
+      SourceLang.value v ->
+      match_states f st1 ->
+      match_states v st2 ->
+      match_states (App f v) (compose_states st1 st2).
+  Proof.
+  Admitted.
+      
+  
+  
+  
     
 End Simulation.
