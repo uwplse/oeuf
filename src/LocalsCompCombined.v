3cf3d9c868b9fc6c5780630e09304a34da5e6e01
Stuart Pernsteiner <spernste@cs.washington.edu>
2016-11-03 10:55:07 -0700

    report in which pass a failure occurred

--- src/LocalsCompCombined.v
+++ src/LocalsCompCombined.v
@@ -2,6 +2,7 @@ Require Import compcert.driver.Compiler compcert.common.Errors.
 Require Import Common Monads.
 Require CompilationUnit.
 Require Import Metadata.
+Require Import CompilerUtil.
 
 Require StackFlatter2 LocalsOnly.
 Require
@@ -15,19 +16,13 @@ Require
 Module A := StackFlatter2.
 Module B := LocalsOnly.
 
-Definition option_to_res {A} (o : option A) : res A :=
-  match o with
-  | None => Error []
-  | Some a => OK a
-  end.
-Coercion option_to_res : option >-> res.
 
 Definition compile_cu (cu : A.env * list metadata) : res (B.env * list metadata) :=
   OK cu
   @@ LocalsDestsComp.compile_cu
   @@ LocalsSwitchComp.compile_cu
   @@ LocalsReturnComp.compile_cu
- @@@ LocalsSourcesComp.compile_cu
+ @@@ LocalsSourcesComp.compile_cu ~~ "LocalsSourcesComp"
   @@ LocalsOnlyComp.compile_cu
 .
 
@@ -64,24 +59,6 @@ eapply IFuncCombined; eassumption.
 Qed.
 
 
-Ltac break_result_chain :=
-    let go l :=
-        match l with
-        | OK _ => fail 1
-        | _ => destruct l eqn:?; try discriminate
-        end in
-    repeat match goal with
-    | [ H : context [ ?l @@ ?r ] |- _ ] => go l
-    | [ H : context [ ?l @@@ ?r ] |- _ ] => go l
-    end.
-
-Ltac inject_ok :=
-    repeat match goal with
-    | [ H : OK ?x = OK ?y |- _ ] =>
-            assert (x = y) by congruence;
-            clear H
-    end.
-
 
 Theorem compile_I_func : forall a ameta b bmeta,
     compile_cu (a, ameta) = OK (b, bmeta) ->
@@ -89,8 +66,7 @@ Theorem compile_I_func : forall a ameta b bmeta,
 intros0 Hcomp. unfold compile_cu in *.
 
 remember (a, ameta) as aprog.
-break_result_chain.  simpl in *.  inject_ok.
-unfold option_to_res in *. repeat (break_match; try discriminate).
+break_result_chain.
 subst aprog.  repeat on (_ * _)%type, fun x => destruct x.
 
 on _, eapply_lem LocalsDestsComp.compile_cu_I_env.
@@ -99,7 +75,6 @@ on _, eapply_lem LocalsReturnComp.compile_cu_I_env.
 on _, eapply_lem LocalsSourcesComp.compile_cu_I_env.
 on _, eapply_lem LocalsOnlyComp.compile_cu_I_env.
 
-inject_ok. inject_pair.
 eapply chain_I_env; eassumption.
 Qed.
 
