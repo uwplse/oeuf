d21446fd12a287585220a404e9a16252f27f44c2
Eric Mullen <emullen@warfa.cs.washington.edu>
2016-12-12 16:16:03 -0800

    sharing the demo

--- compile_demo.sh
+++ compile_demo.sh
@@ -7,8 +7,8 @@ set -e
 COQC="coqc $(sed '/^[[:space:]]*$/q' _CoqProject)"
 
 #compile demo.v to get .oeuf file
-$COQC -Q src "" "test/demo.v" >/dev/null 2>&1 || { echo "coqc failed on $f"; exit 1; }
+$COQC -Q src "" "test/List_Demo.v" >/dev/null 2>&1 || { echo "coqc failed on List_Demo.v"; exit 1; }
 
 #use Oeuf driver to get compile .oeuf file
-./occ.sh demo >/dev/null 2>&1 || { echo "occ.sh failed on demo"; exit 1; }
+./occ.sh list_demo >/dev/null 2>&1 || { echo "occ.sh failed on list_demo"; exit 1; }
 
