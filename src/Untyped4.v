511b847437a9514400db64a4fa86edb457192539
Stuart Pernsteiner <spernste@cs.washington.edu>
2017-01-24 11:41:14 -0800

    untyped4 - split to arg + upvar

--- src/Untyped4.v
+++ src/Untyped4.v
@@ -5,17 +5,12 @@ Require Import Metadata.
 Require Import Semantics.
 Require Import HighestValues.
 
-(* TODO:
- - directly run_cont from SConstrDone/SCloseDone
- - unflag values
- - convert to functional continuations
- *)
-
 
 
 Inductive expr :=
 | Value (v : value)
-| Var (idx : nat)
+| Arg
+| UpVar (idx : nat)
 | App (f : expr) (a : expr)
 | MkConstr (ctor : constr_name) (args : list expr)
 | MkClose (fname : nat) (free : list expr)
@@ -41,69 +36,6 @@ Inductive state :=
 | Run (e : expr) (l : list value) (k : cont)
 | Stop (v : value)
 .
-
-
-Definition weaken_value :=
-    let fix go v :=
-        let fix go_list vs :=
-            match vs with
-            | [] => []
-            | v :: vs => go v :: go_list vs
-            end in
-        match v with
-        | Constr ctor args => Constr ctor (go_list args)
-        | Close fname free => Close (S fname) (go_list free)
-        end in go.
-
-Definition weaken_value_list :=
-    let go := weaken_value in
-    let fix go_list es :=
-        match es with
-        | [] => []
-        | e :: es => go e :: go_list es
-        end in go_list.
-
-Definition weaken_expr :=
-    let fix go e :=
-        let fix go_list es :=
-            match es with
-            | [] => []
-            | e :: es => go e :: go_list es
-            end in
-        match e with
-        | Value v => Value (weaken_value v)
-        | Var idx => Var idx
-        | App f a => App (go f) (go a)
-        | MkConstr ctor args => MkConstr ctor (go_list args)
-        | MkClose fname free => MkClose (S fname) (go_list free)
-        | Elim ty cases target => Elim ty (go_list cases) (go target)
-        end in go.
-
-Definition weaken_expr_list :=
-    let go := weaken_expr in
-    let fix go_list es :=
-        match es with
-        | [] => []
-        | e :: es => go e :: go_list es
-        end in go_list.
-
-Fixpoint get_weaken g n :=
-    match n with
-    | 0 =>
-            match g with
-            | [] => None
-            | e :: _ => Some (weaken_expr e)
-            end
-    | S n' =>
-            match g with
-            | [] => None
-            | _ :: g' =>
-                    match get_weaken g' n' with
-                    | Some e => Some (weaken_expr e)
-                    | None => None
-                    end
-            end
-    end.
 
 
 (* helper function for proceeding into a continuation *)
@@ -139,25 +71,20 @@ Fixpoint unroll_elim' (case : expr)
 Definition unroll_elim case ctor args mk_rec :=
     unroll_elim' case ctor args mk_rec 0.
 
-Definition run_elim (ty : type_name) (cases : list expr) (target : value) :=
-    match target with
-    | Constr ctor args =>
-            match nth_error cases (constructor_index ctor) with
-            | Some case => Some (unroll_elim case ctor args (Elim ty cases))
-            | None => None
-            end
-    | _ => None
-    end.
-
 (* the actual step relation *)
 Inductive sstep (g : list expr) : state -> state -> Prop :=
 | SValue : forall v (l : list value) (k : cont),
         sstep g (Run (Value v) l k)
                 (run_cont k v)
 
-| SVar : forall idx (l : list value) (k : cont) v,
-        nth_error l idx = Some v ->
-        sstep g (Run (Var idx) l k)
+| SArg : forall (l : list value) (k : cont) v,
+        nth_error l 0 = Some v ->
+        sstep g (Run (Arg) l k)
+                (Run (Value v) l k)
+
+| SUpVar : forall idx (l : list value) (k : cont) v,
+        nth_error l (S idx) = Some v ->
+        sstep g (Run (UpVar idx) l k)
                 (Run (Value v) l k)
 
 | SAppL : forall (e1 : expr) (e2 : expr) l k,
@@ -172,7 +99,7 @@ Inductive sstep (g : list expr) : state -> state -> Prop :=
                 (Run e2 l (KAppR e1 l k))
 
 | SMakeCall : forall fname free arg l k body,
-        get_weaken g fname = Some body ->
+        nth_error g fname = Some body ->
         sstep g (Run (App (Value (Close fname free)) (Value arg)) l k)
                 (Run body (arg :: free) k)
 
@@ -228,11 +155,13 @@ Inductive sstep (g : list expr) : state -> state -> Prop :=
             (cases : list expr)
             (ctor : constr_name)
             (args : list value)
+            (case : expr)
             (result : expr)
             l k,
         is_ctor_for_type ty ctor ->
         constructor_arg_n ctor = length args ->
-        run_elim ty cases (Constr ctor args) = Some result ->
+        nth_error cases (constructor_index ctor) = Some case ->
+        unroll_elim case ctor args (Elim ty cases) = result ->
         sstep g (Run (Elim ty cases (Value (Constr ctor args))) l k)
                 (Run result l k)
 .
---
+++ src/UntypedComp4.v
@@ -0,0 +1,222 @@
+Require Import Common.
+
+Require Import Utopia.
+Require Import Metadata.
+Require Import Program.
+
+Require Import ListLemmas.
+Require Import HList.
+Require Import CompilationUnit.
+Require Import Semantics.
+Require Import HighestValues.
+
+Require Untyped1.
+Require Untyped3.
+Require Untyped4.
+
+Module A := Untyped3.
+Module AS := Untyped1.
+Module B := Untyped4.
+
+
+Definition compile_expr :=
+    let fix go e :=
+        let fix go_list es :=
+            match es with
+            | [] => []
+            | e :: es => go e :: go_list es
+            end in
+        match e with
+        | AS.Value v => B.Value v
+        | AS.Var 0 => B.Arg
+        | AS.Var (S idx) => B.UpVar idx
+        | AS.App f a => B.App (go f) (go a)
+        | AS.MkConstr ctor args => B.MkConstr ctor (go_list args)
+        | AS.MkClose fname free => B.MkClose fname (go_list free)
+        | AS.Elim ty cases target => B.Elim ty (go_list cases) (go target)
+        end in go.
+
+Definition compile_expr_list :=
+    let go := compile_expr in
+    let fix go_list es :=
+        match es with
+        | [] => []
+        | e :: es => go e :: go_list es
+        end in go_list.
+
+Definition compile_genv := compile_expr_list.
+
+
+Inductive I_expr : AS.expr -> B.expr -> Prop :=
+| IValue : forall v, I_expr (AS.Value v) (B.Value v)
+| IArg : I_expr (AS.Var 0) (B.Arg)
+| IUpVar : forall idx, I_expr (AS.Var (S idx)) (B.UpVar idx)
+| IApp : forall af aa bf ba,
+        I_expr af bf ->
+        I_expr aa ba ->
+        I_expr (AS.App af aa) (B.App bf ba)
+| IMkConstr : forall ctor aargs bargs,
+        Forall2 I_expr aargs bargs ->
+        I_expr (AS.MkConstr ctor aargs) (B.MkConstr ctor bargs)
+| IMkClose : forall fname afree bfree,
+        Forall2 I_expr afree bfree ->
+        I_expr (AS.MkClose fname afree) (B.MkClose fname bfree)
+| IElim : forall ty acases atarget bcases btarget,
+        Forall2 I_expr acases bcases ->
+        I_expr atarget btarget ->
+        I_expr (AS.Elim ty acases atarget) (B.Elim ty bcases btarget)
+.
+
+Inductive I_cont : AS.cont -> B.cont -> Prop :=
+| IKAppL : forall l  ae2 ak  be2 bk,
+        I_expr ae2 be2 ->
+        I_cont ak bk ->
+        I_cont (AS.KAppL ae2 l ak) (B.KAppL be2 l bk)
+| IKAppR : forall l  ae1 ak  be1 bk,
+        I_expr ae1 be1 ->
+        I_cont ak bk ->
+        I_cont (AS.KAppR ae1 l ak) (B.KAppR be1 l bk)
+| IKConstr : forall ctor l  avs aes ak  bvs bes bk,
+        Forall2 I_expr avs bvs ->
+        Forall2 I_expr aes bes ->
+        I_cont ak bk ->
+        I_cont (AS.KConstr ctor avs aes l ak)
+               (B.KConstr ctor bvs bes l bk)
+| IKClose : forall fname l  avs aes ak  bvs bes bk,
+        Forall2 I_expr avs bvs ->
+        Forall2 I_expr aes bes ->
+        I_cont ak bk ->
+        I_cont (AS.KClose fname avs aes l ak)
+               (B.KClose fname bvs bes l bk)
+| IKElim : forall ty l  acases ak  bcases bk,
+        Forall2 I_expr acases bcases ->
+        I_cont ak bk ->
+        I_cont (AS.KElim ty acases l ak)
+               (B.KElim ty bcases l bk)
+.
+
+Inductive I : AS.state -> B.state -> Prop :=
+| IRun : forall l  ae ak  be bk,
+        I_expr ae be ->
+        I_cont ak bk ->
+        I (AS.Run ae l ak) (B.Run be l bk)
+| IStop : forall v,
+        I (AS.Stop v) (B.Stop v)
+.
+
+
+
+Ltac i_ctor := intros; constructor; eauto.
+Ltac i_lem H := intros; eapply H; eauto.
+
+Lemma I_run_cont : forall ak bk v,
+    I_cont ak bk ->
+    I (AS.run_cont ak v) (B.run_cont bk v).
+intros0 II; invc II; repeat i_ctor.
+- i_lem Forall2_app. i_ctor. i_ctor.
+- i_lem Forall2_app. i_ctor. i_ctor.
+Qed.
+
+Lemma I_is_value : forall a b,
+    I_expr a b ->
+    AS.is_value a ->
+    B.is_value b.
+intros0 II Aval; invc Aval; invc II; i_ctor.
+Qed.
+Hint Resolve I_is_value.
+
+Lemma I_isnt_value : forall a b,
+    I_expr a b ->
+    ~ AS.is_value a ->
+    ~ B.is_value b.
+intros0 II Hval; contradict Hval; invc Hval; invc II; i_ctor.
+Qed.
+Hint Resolve I_isnt_value.
+
+Lemma I_expr_map_value : forall vs bes,
+    Forall2 I_expr (map AS.Value vs) bes ->
+    bes = map B.Value vs.
+induction vs; destruct bes; intros0 II; invc II; simpl.
+- reflexivity.
+- on (I_expr _ _), invc.
+  f_equal. i_lem IHvs.
+Qed.
+
+
+Lemma I_unroll_elim' : forall acase bcase ctor args amk_rec bmk_rec idx,
+    I_expr acase bcase ->
+    (forall a b, I_expr a b -> I_expr (amk_rec a) (bmk_rec b)) ->
+    I_expr (AS.unroll_elim' acase ctor args amk_rec idx)
+           (B.unroll_elim' bcase ctor args bmk_rec idx).
+first_induction args; intros0 IIcase IImk_rec; simpl.
+- assumption.
+- break_if.
+  + eapply IHargs; eauto. repeat i_ctor. eapply IImk_rec. i_ctor.
+  + eapply IHargs; eauto. repeat i_ctor.
+Qed.
+
+Lemma I_unroll_elim : forall acase bcase ctor args amk_rec bmk_rec,
+    I_expr acase bcase ->
+    (forall a b, I_expr a b -> I_expr (amk_rec a) (bmk_rec b)) ->
+    I_expr (AS.unroll_elim acase ctor args amk_rec)
+           (B.unroll_elim bcase ctor args bmk_rec).
+intros. eapply I_unroll_elim'; auto.
+Qed.
+
+
+Theorem I_sim : forall AE BE a a' b,
+    Forall2 I_expr AE BE ->
+    I a b ->
+    A.sstep AE a a' ->
+    exists b',
+        B.sstep BE b b' /\
+        I a' b'.
+destruct a as [ae l ak | v];
+intros0 Henv II Astep; inv Astep.
+all: invc II.
+all: try on (I_expr (_ _) be), invc.
+
+all: try solve [eexists; split; repeat i_ctor].
+
+- (* SValue *)
+  eexists. split. i_lem B.SValue.
+  + i_lem I_run_cont.
+
+- (* SMakeCall *)
+  on (I_expr _ bf), invc.
+  on (I_expr _ ba), invc.
+  fwd eapply Forall2_nth_error_ex as HH; eauto.  destruct HH as (bbody & ? & ?).
+  eexists. split. i_lem B.SMakeCall.
+  i_ctor.
+
+- (* SConstrStep *)
+  on (Forall2 _ (_ ++ _) _), invc_using Forall2_app_inv.
+  on (Forall2 _ (_ :: _) _), invc.
+  eexists. split. i_lem B.SConstrStep.
+  + list_magic_on (vs, (ys1, tt)).
+  + i_ctor. i_ctor.
+
+- (* SConstrDone *)
+  erewrite I_expr_map_value with (bes := bargs) by eauto.
+  eexists. split. i_lem B.SConstrDone.
+  i_ctor. i_ctor.
+
+- (* SCloseStep *)
+  on (Forall2 _ (_ ++ _) _), invc_using Forall2_app_inv.
+  on (Forall2 _ (_ :: _) _), invc.
+  eexists. split. i_lem B.SCloseStep.
+  + list_magic_on (vs, (ys1, tt)).
+  + i_ctor. i_ctor.
+
+- (* SCloseDone *)
+  erewrite I_expr_map_value with (bes := bfree) by eauto.
+  eexists. split. i_lem B.SCloseDone.
+  i_ctor. i_ctor.
+
+- (* SEliminate *)
+  on (I_expr _ btarget), invc.
+  fwd eapply Forall2_nth_error_ex as HH; eauto.  destruct HH as (bcase & ? & ?).
+  eexists. split. i_lem B.SEliminate.
+  i_ctor. i_lem I_unroll_elim. i_ctor.
+Qed.
+
