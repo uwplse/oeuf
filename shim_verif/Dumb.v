97761dceb30b1864b8ad3ade466dad17480127a9
Eric Mullen <emullen@cs.washington.edu>
2017-02-09 23:57:11 -0800

    I think I have a program, but I don't know if I actually do, and I can't figure out how to make it into a compilation unit

--- shim_verif/Dumb.v
+++ shim_verif/Dumb.v
@@ -4,38 +4,57 @@ Definition id (x : nat) : nat := x.
 Definition zero : nat := O.
 
 Require Pretty CompilationUnit.
-Require Import SourceLang.
+Require Import SourceLifted.
 Require OeufPlugin.OeufPlugin.
-
-Definition id_ty : type := ltac:(type_reflect (nat -> nat)).
-Definition zero_ty : type := ltac:(type_reflect (nat)).
-
-Definition id_reflect {l} : expr l id_ty :=
-  ltac:(let x := eval unfold id in id in reflect x).
-
-Definition zero_reflect {l} : expr l zero_ty :=
-  ltac:(let x := eval unfold zero in zero in reflect x).
-
-Lemma id_reflect_correct :
-  forall l h,
-    expr_denote (l := l) id_reflect h = id.
-Proof.
-  reflexivity.
-Qed.
-
-Lemma zero_reflect_correct :
-  forall l h,
-    expr_denote (l := l) zero_reflect h = zero.
-Proof.
-  reflexivity.
-Qed.
-
-
 Require Import List.
 Import List.ListNotations.
 Import HList.
 Require Import String.
+Require Import Utopia.
 
+Definition id_ty : type := Arrow (ADT Utopia.Tnat) (ADT Utopia.Tnat).
+Definition zero_ty : type := ADT Utopia.Tnat.
+Definition N := ADT Tnat.
+
+Definition id_G : list (type *  list type * type) := [(N, [], N); (N, [], N)].
+
+Definition id_sl  {l} : expr id_G l id_ty.
+  eapply Close. unfold id_G. eapply Here; eauto.
+  econstructor; eauto.
+Defined.
+
+Definition zero_sl {l} : expr id_G l zero_ty.
+  eapply Constr. eapply CTO. econstructor; eauto.
+Defined.
+
+Definition id_id : body_expr (skipn 2 id_G) (N, [], N).
+  simpl. eapply Var. econstructor; eauto.
+Defined.
+
+Definition zero_zero : body_expr (skipn 1 id_G) (N, [], N).
+  simpl. eapply Value. eapply VConstr.
+  eapply CTO. econstructor; eauto.
+Defined.
+
+Definition id_genv : genv id_G :=
+  (GenvCons zero_zero
+  (GenvCons id_id
+            (GenvNil))).
+            
+
+Definition zero_denoted := hhead (genv_denote id_genv) hnil.
+
+Lemma zero_denote_same :
+  forall x,
+    zero_denoted x = O.
+Proof.
+  reflexivity.
+Qed.
+
+
+Print CompilationUnit.CompilationUnit.
+
+(*
 Definition oeuf_prog := (CompilationUnit.CompilationUnit _ (hcons (@id_reflect [])
                                           (hcons (@zero_reflect []) hnil))
                                         ["id"; "zero"]%list%string).
@@ -43,3 +62,4 @@ Definition oeuf_prog := (CompilationUnit.CompilationUnit _ (hcons (@id_reflect [
 Oeuf Eval lazy Then Write To File "dumb.oeuf"
      (Pretty.compilation_unit.pretty 75
      oeuf_prog).
+*)
