cc63c73f4c5087c6c7873858f1d64c50bafdf821
Eric A Mullen <emullen@cs.washington.edu>
2017-02-13 22:40:33 -0800

    using linker to add some external functions, will make genv swapping easier

--- shim_verif/dumb_proof.v
+++ shim_verif/dumb_proof.v
@@ -10,19 +10,33 @@ Require Import compcert.common.Values.
 Require Import compcert.lib.Coqlib.
 Require Import compcert.common.Events.
 Require Import compcert.common.Smallstep.
+Require Import Semantics.
 Require Import compcert.backend.Cminor.
 (* prog is the whole program *)
 
 Require Import StructTact.StructTactics.
 Require Import StructTact.Util.
 
+Require Import NewCont.
 Require Import EricTact.
+Require Import StuartTact.
+
 Require Import OeufProof.
+
 
 Require Cmajor.
 
 Require Import CminorLib.
-  
+
+Lemma star_to_star :
+  forall {genv state : Type} step (ge : genv) (st : state) t st',
+    TraceSemantics.star step ge st t st' <-> Smallstep.star step ge st t st'.
+Proof.
+  split;
+    induction 1; intros;
+      econstructor; eauto.
+Qed.
+
 
 Section SIM.
 
@@ -32,7 +46,7 @@ Section SIM.
   Variable st : Cminor.state.
   Hypothesis init_state : initial_state prog st.
 
-
+  
   Lemma steps :
     exists st1,
       Smallstep.star step ge st E0 st1.
@@ -58,6 +72,13 @@ Section SIM.
     take_step.
     take_step.
     alloc.
+    store_step.
+    take_step.
+    take_step.
+    take_step.
+    take_step.
+    take_step.
+    store_step.
     take_step.
     take_step.
     take_step.
@@ -66,36 +87,18 @@ Section SIM.
     store_step.
     take_step.
     take_step.
-    store_step. 
-    take_step.
-    free_step.
     take_step.
     take_step.
     take_step.
-    take_step.
-    take_step.
-    take_step.
-    take_step.
-    alloc.
-    take_step.
-    take_step.
-    take_step.
-    take_step.
-    alloc.
-    store_step.
-    take_step.
-    take_step.
-    assert (Genv.find_symbol ge _id_lambda0 = Some 3%positive).
+    assert (Genv.find_symbol ge _id = Some 1%positive).
     {
-      unfold Genv.find_symbol. unfold ge. simpl. reflexivity.
+      unfold Genv.find_symbol. simpl. reflexivity.
     } idtac.
+
     store_step.
-    find_rewrite.
-    take_step.
-    free_step.
-    take_step.
-    take_step.
-    take_step.
+    Focus 4. simpl.
+    eauto.
+    usable_chain. store_auto. store_auto.
     take_step.
     take_step.
     take_step.
@@ -103,135 +106,176 @@ Section SIM.
     alloc.
     take_step.
     take_step.
-
-
-
     load_step.
-    (* b2 <> b3 *)
-    copy Heqp2.
-    eapply Mem.nextblock_alloc in Heqp2.
-    copy Heqp3.
-    eapply Mem.nextblock_alloc in Heqp3.
-    eapply Mem.alloc_result in H5. eapply Mem.alloc_result in H6.
-    rewrite <- H5 in *. rewrite <- H6 in *.
-    assert (Plt b2 b3). subst. eapply Plt_succ.
-    intro. symmetry in H8. eapply Plt_ne; eauto.
-    
-    simpl. unfold Integers.Int.zero. break_match; try congruence.
-    unfold ge. unfold Genv.find_funct_ptr. simpl. reflexivity.
-    simpl. reflexivity.
+    (* TODO: wrap following stuff in load_step *)
+    unfold Genv.find_funct.
+    unfold Genv.find_funct_ptr.
+    simpl. unfold Integers.Int.zero.
+    break_match; try congruence. reflexivity.
+    reflexivity.
+    (* end load_step stuff *)
 
+    (* HERE is where we call into Oeuf *)
+    
     (* This is the complicated continuation we've built up *)
     (* We'll need this later, after we're back from oeuf *)
-    remember ((Kcall (Some 125%positive) f_call (Values.Vptr b4 Integers.Int.zero)
+    remember (Kcall (Some 123%positive) f_call (Vptr b2 (Integers.Int.repr 0))
             (set_locals (fn_vars f_call)
-               (set_params (Values.Vptr b3 Integers.Int.zero :: Values.Vptr b1 Integers.Int.zero :: nil) (fn_params f_call)))
-            (Kseq (Sreturn (Some (Evar 125%positive)))
-               (Kcall (Some 131%positive) f_main (Values.Vptr b Integers.Int.zero)
-                  (Maps.PTree.set _id_closure (Values.Vptr b3 Integers.Int.zero)
-                     (set_optvar (Some 130%positive) (Values.Vptr b3 Integers.Int.zero)
-                        (Maps.PTree.set _zero_value (Values.Vptr b1 Integers.Int.zero)
-                           (set_optvar (Some 129%positive) (Values.Vptr b1 Integers.Int.zero)
-                              (set_locals (fn_vars f_main) (set_params nil (fn_params f_main)))))))
-                  (Kseq (Sassign _result (Evar 131%positive))
+               (set_params
+                  (Vptr b1 (Integers.Int.repr 0)
+                   :: Vptr b0 (Integers.Int.repr 0) :: nil) 
+                  (fn_params f_call)))
+            (Kseq (Sreturn (Some (Evar 123%positive)))
+               (Kcall (Some 129%positive) f_main (Vptr b (Integers.Int.repr 0))
+                  (Maps.PTree.set _id_closure (Vptr b1 (Integers.Int.repr 0))
+                     (set_optvar (Some 128%positive) (Vptr b1 (Integers.Int.repr 0))
+                        (Maps.PTree.set _zero_value (Vptr b0 (Integers.Int.repr 0))
+                           (set_optvar (Some 127%positive)
+                              (Vptr b0 (Integers.Int.repr 0))
+                              (set_locals (fn_vars f_main)
+                                 (set_params nil (fn_params f_main)))))))
+                  (Kseq (Sassign _result (Evar 129%positive))
                      (Kseq
                         (Sseq
                            (Sseq
-                              (Scall (Some 132%positive)
+                              (Scall (Some 130%positive)
                                  {|
                                  AST.sig_args := AST.Tint :: nil;
                                  AST.sig_res := Some AST.Tint;
-                                 AST.sig_cc := AST.cc_default |} (Econst (Oaddrsymbol _read_nat (Integers.Int.repr 0)))
+                                 AST.sig_cc := AST.cc_default |}
+                                 (Econst
+                                    (Oaddrsymbol _read_nat (Integers.Int.repr 0)))
                                  (Evar _result :: nil))
                               (Scall None
                                  {|
                                  AST.sig_args := AST.Tint :: AST.Tint :: nil;
                                  AST.sig_res := Some AST.Tint;
-                                 AST.sig_cc := {| AST.cc_vararg := true; AST.cc_unproto := false; AST.cc_structret := false |} |}
+                                 AST.sig_cc := {|
+                                               AST.cc_vararg := true;
+                                               AST.cc_unproto := false;
+                                               AST.cc_structret := false |} |}
                                  (Econst (Oaddrsymbol _printf (Integers.Int.repr 0)))
-                                 (Econst (Oaddrsymbol ___stringlit_3 (Integers.Int.repr 0)) :: Evar 132%positive :: nil)))
-                           (Sreturn (Some (Econst (Ointconst (Integers.Int.repr 0))))))
-                        (Kseq (Sreturn (Some (Econst (Ointconst (Integers.Int.repr 0))))) Kstop))))))) as K.
-    
-    (* HERE is where we call into Oeuf *)
-    (* This is the state that we want to be a callstate *)
-    remember ((Callstate (AST.Internal f_id_lambda0) (Values.Vptr b3 Integers.Int.zero :: Values.Vptr b1 Integers.Int.zero :: nil)
-                         Kstop m5)) as OST. (* Oeuf state *)
+                                 (Econst
+                                    (Oaddrsymbol ___stringlit_3
+                                       (Integers.Int.repr 0))
+                                  :: Evar 130%positive :: nil)))
+                           (Sreturn
+                              (Some (Econst (Ointconst (Integers.Int.repr 0))))))
+                        (Kseq
+                           (Sreturn
+                              (Some (Econst (Ointconst (Integers.Int.repr 0)))))
+                           Kstop)))))) as K.
 
-    remember ((Callstate (AST.Internal f_id_lambda0) (Values.Vptr b3 Integers.Int.zero :: Values.Vptr b1 Integers.Int.zero :: nil)
-                         K m5)) as LST. (* linked state *)
+    (* give nice names to the oeuf and linked states *)
+    remember (Callstate (AST.Internal f_id) (Vptr b1 (Integers.Int.repr 0) :: Vptr b0 (Integers.Int.repr 0) :: nil) K m3) as LST.
+    remember (Callstate (AST.Internal f_id) (Vptr b1 (Integers.Int.repr 0) :: Vptr b0 (Integers.Int.repr 0) :: nil) Kstop m3) as OST.
+    
 
     (* make sure it's a callstate *)
-    assert (Cmajor.cminor_is_callstate oprog (HighValues.Close _id_lambda0 nil) (HighValues.Constr Integers.Int.zero nil) OST).
+    assert (Cmajor.cminor_is_callstate oprog (HighValues.Close _id nil) (HighValues.Constr Integers.Int.zero nil) OST).
     {
+      assert (b1 <> b0). {
+        admit.
+      } idtac.
       subst. econstructor.
       econstructor. Focus 3. unfold Genv.find_symbol. simpl. reflexivity.
       Focus 2. unfold Genv.find_funct_ptr. simpl. reflexivity.
       eapply loadable_load. simpl_int_add. loadable_chain.
-
-      (* b3 <> b2 *)
-      copy Heqp2.
-      eapply Mem.nextblock_alloc in Heqp2.
-      copy Heqp3.
-      eapply Mem.nextblock_alloc in Heqp3.
-      eapply Mem.alloc_result in H5. eapply Mem.alloc_result in H6.
-      rewrite <- H5 in *. rewrite <- H6 in *.
-      assert (Plt b2 b3). subst. eapply Plt_succ.
-      intro. symmetry in H8. eapply Plt_ne; eauto.
-
-      
       simpl. reflexivity.
-      intros. simpl in H5. inv H5.
-      econstructor.
+      intros. simpl in *. invp False.
+      
+      econstructor. 
+      
+      eapply loadable_load. simpl_int_add. loadable_chain.
+      simpl. reflexivity.
+      intros. simpl in *. invp False.
 
       eapply loadable_load. simpl_int_add. loadable_chain.
-
-      (* b1 <> b0 *)
-      copy Heqp0.
-      eapply Mem.nextblock_alloc in Heqp0.
-      copy Heqp1.
-      eapply Mem.nextblock_alloc in Heqp1.
-      eapply Mem.alloc_result in H5. eapply Mem.alloc_result in H6.
-      rewrite <- H5 in *. rewrite <- H6 in *.
-      assert (Plt b0 b1). subst. eapply Plt_succ.
-      intro. symmetry in H8. eapply Plt_ne; eauto.
-      admit.
-      admit.
-      admit.
-      simpl. reflexivity.
-      intros. simpl in H5. inv H5.
-      Focus 3. unfold Genv.find_symbol. simpl. reflexivity.
-      Focus 2. unfold Genv.find_funct_ptr. simpl. reflexivity.
-
-      admit.
+      unfold Genv.find_funct_ptr. simpl. reflexivity.
+      unfold Genv.find_symbol. simpl. reflexivity.
       simpl. reflexivity.
 
-      admit. (* True, but mem fact *)
+      admit. (* global_blocks_valid *)
+      admit. (* no_future_pointers *)
 
-      admit. (* True, but deeper mem fact *)
-      
+      econstructor; eauto. simpl. left. reflexivity.
+      econstructor. eauto.
+
     } idtac.
 
-    (* TODO above: *)
-    (* 1. make a better way to prove blocks disjoint *)
-    (* 2. global_blocks_valid and no_future_pointers chaining lemmas *)
     
-    (* We need a handle on the compilation unit name *)
-    (*
-    eapply OeufProof.establish_matching in H6.
-    Focus 2.
-    instantiate (4 := Dumb.oeuf_prog).
-    instantiate (3 := dumb_axioms.TRANSF).
-    instantiate (2 := Dumb.id_ty).
-    instantiate (1 := Dumb.id_reflect).
-    apply OeufProof.match_vals_values; econstructor; eauto.
-    Focus 2.
-    instantiate (2 := Dumb.zero_ty).
-    instantiate (1 := Dumb.id_reflect).
+    remember (@SourceLifted.VConstr id_G _ _ _ SourceLifted.CTO HList.hnil) as SZero.
+    remember (@SourceLifted.VClose id_G _ _ _ HList.Here HList.hnil) as SID.
     
-      try solve [].
-    instantiate (3 := Dumb.oeuf_prog) in H6.    
-     *)
+    (* establish matching callstates *)
+    copy H5.
+    eapply (OeufProof.oeuf_match_callstate Dumb.oeuf_prog _ dumb_axioms.TRANSF) in H5.
+    
+    Focus 3. 
+    instantiate (1 := SZero).
+    econstructor; eauto. split. reflexivity.
+    admit. (* TODO: make sure we can prove this *)
+    Focus 2. instantiate (1 := SID).
+    econstructor; eauto. split. reflexivity.
+    admit. (* TODO: make sure we can prove this *)
+    repeat progress (try break_exists; try break_and).
+
+
+    
+    (* use matching states to step *)
+    eapply OeufProof.oeuf_star_simulation in H5.
+    Focus 2. subst SID. subst SZero.
+
+    clear -H7. inversion H7.
+
+    subst ret_ty. 
+    eapply existT_eq in H13.
+    subst free_tys.
+    eapply existT_eq in H13.
+    eapply existT_eq in H13.
+    subst mb0.
+    eapply existT_eq in H14. subst free0.
+    eapply existT_eq in H17. subst x3.
+    eapply existT_eq in H15. subst av.
+    all: try solve [try eapply list_eq_dec; eapply SourceLifted.type_eq_dec].
+
+    eapply star_left.
+    econstructor; eauto.
+    eapply star_left.
+    econstructor; eauto.
+    eapply star_refl.
+
+
+    assert (SourceLifted.final_state (SourceLifted.run_cont SourceLifted.KStop (HList.hget (HList.hcons SZero HList.hnil) HList.Here)) SZero). {
+      econstructor; eauto.
+    } idtac.
+
+    repeat progress (try break_exists; try break_and).
+    eapply OeufProof.oeuf_match_final_states in H9; try eassumption.
+    break_exists; break_and.
+    
+
+    eapply subst_in_cont in H5; try eassumption.
+    instantiate (1 := K) in H5. unfold NewCont.ge in H5.
+    repeat break_exists. repeat break_and.
+    Focus 2. subst K. econstructor; eauto.
+    assert (x8 = LST). {
+      subst LST OST. inv H5.
+      f_equal.
+      invp match_cont. reflexivity.
+    }
+    subst x8.
+
+    
+    
+    eapply star_to_star in H11.
+
+    (* Here we have the correct state, we only need to change up the global environment *)
+
+    
+      
+      
+    
     
 
   Admitted.
