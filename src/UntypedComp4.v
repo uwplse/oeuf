761bdb976d57ffb14524e2a4cd7c98f6a56f9b0b
Eric A Mullen <emullen@cs.washington.edu>
2017-02-16 15:00:05 -0800

    starting proving match val eq

--- src/UntypedComp4.v
+++ src/UntypedComp4.v
@@ -286,7 +286,20 @@ Section Preservation.
 
     - intros0 Astep. intros0 II.
       i_lem I_sim.
+    Defined.
 
+    Lemma match_val_eq :
+      Semantics.fsim_match_val _ _ fsim = eq.
+    Proof.
+      unfold fsim. simpl.
+      unfold Semantics.fsim_match_val.
+      break_match. repeat (break_match_hyp; try congruence).
+      try unfold forward_simulation_step in *.
+      try unfold forward_simulation_plus in *.
+      try unfold forward_simulation_star in *.
+      try unfold forward_simulation_star_wf in *.
+      inv Heqf. reflexivity.
     Qed.
+
 
 End Preservation.
