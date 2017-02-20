388db38de54a625f78d57c279ea2265356b06bae
Eric A Mullen <emullen@cs.washington.edu>
2017-02-20 15:29:47 -0800

    killing admits, more gonna die soon

--- shim_verif/dumb_axioms.v
+++ shim_verif/dumb_axioms.v
@@ -2,9 +2,12 @@ Require dumb_cm.
 Require dumb_oeuf.
 Require Dumb.
 Require Oeuf.
+Require Linker.
 
 (* Here we will list all the axioms we need *)
 
 (* *)
 Axiom TRANSF : Oeuf.transf_oeuf_to_cminor Dumb.oeuf_prog = Errors.OK dumb_oeuf.prog.
 
+Axiom shim_prog : Cminor.program.
+Axiom LINKED : Linker.shim_link dumb_oeuf.prog shim_prog = Errors.OK dumb_cm.prog.
