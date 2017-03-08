80537bbc450f7903e4f7a7e4d463f88cee92fd9f
Eric A Mullen <emullen@cs.washington.edu>
2017-03-08 10:28:59 -0800

    it builds, moving to warfa

--- demos/sha256/SHA256_AST.v
+++ demos/sha256/SHA256_AST.v
@@ -10,7 +10,13 @@ Require Import NArith.
 Require Import SHA256_elim.
 
 Require Import OeufPlugin.OeufPlugin.
+Require Import Pretty.
 
 Require Import String.
 
 Time Definition sha_256_cu : compilation_unit := ltac:(oeuf_reflect SHA_256).
+
+
+Oeuf Eval lazy Then Write To File "sha256.oeuf"
+     (Pretty.compilation_unit.print 
+       sha_256_cu).
