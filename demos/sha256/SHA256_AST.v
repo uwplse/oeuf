afb4c42e5e918a7f24d89863eb5627787750f4ea
Stuart Pernsteiner <spernste@cs.washington.edu>
2017-03-08 15:28:10 -0800

    split sha256.oeuf printing into several parts

--- demos/sha256/SHA256_AST.v
+++ demos/sha256/SHA256_AST.v
@@ -11,12 +11,14 @@ Require Import SHA256_elim.
 
 Require Import OeufPlugin.OeufPlugin.
 Require Import Pretty.
+From PrettyParsing Require Import PrettyParsing.
 
 Require Import String.
 
 Time Definition sha_256_cu : compilation_unit := ltac:(oeuf_reflect SHA_256).
 
+Time Definition sha_256_tree :=
+    Eval compute in Pretty.compilation_unit.to_tree sha_256_cu.
 
-Oeuf Eval lazy Then Write To File "sha256.oeuf"
-     (Pretty.compilation_unit.print 
-       sha_256_cu).
+Time Oeuf Eval lazy Then Write To File "sha256.oeuf"
+     (print_tree sha_256_tree).
