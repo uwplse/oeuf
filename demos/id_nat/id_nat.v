c41d173005de669ca897c99f95150a1c33822433
Eric A Mullen <emullen@cs.washington.edu>
2018-01-04 10:27:42 -0800

    demo is simpler, plugin now sets printing all internally

--- demos/id_nat/id_nat.v
+++ demos/id_nat/id_nat.v
@@ -1,50 +1,59 @@
 Require Import Arith.
 Require Import Omega.
 Require Import List.
+Require Import String.
 Import ListNotations.
 
+Require Import Oeuf.
 
+(* Our core coq function *)
 Fixpoint id_nat (x : nat) :=
     match x with
     | 0 => 0
     | S n => S (id_nat n)
     end.
 
-Theorem id_nat_id : forall x, id_nat x = x.
-induction x.
-- reflexivity.
-- simpl. f_equal. eauto.
+(* Proof of correctness of our function *)
+Theorem id_nat_id :
+  forall x, id_nat x = x.
+Proof.
+  induction x.
+  - reflexivity.
+  - simpl. f_equal. eauto.
 Qed.
 
-
-
+(* Core function in terms of eliminators *)
 Definition id_nat_elim (x : nat) :=
-    @nat_rect (fun _ => nat)
-        (0)
-        (fun n id_nat_elim_n => S id_nat_elim_n)
-        x.
+  @nat_rect
+    (fun _ => nat)
+    0
+    (fun n' rec_res => S rec_res)
+    x.
 
-Theorem id_nat_elim_id_nat : id_nat_elim = id_nat.
-reflexivity.
+(* Proof that elim version is same as original *)
+Theorem id_nat_elim_id_nat :
+  id_nat_elim = id_nat.
+Proof.
+  reflexivity.
 Qed.
 
-
-
-
-Require String.
-Require Import oeuf.HList oeuf.Utopia oeuf.SourceLifted oeuf.SourceValues oeuf.CompilationUnit.
-Require Import OeufPlugin.OeufPlugin.
-
-Set Printing All.
+(* Reflect id_nat_elim *)
 Oeuf Reflect id_nat_elim As id_nat_cu.
-Unset Printing All.
 
+(* Look at what's reflected *)
 Check id_nat_cu : compilation_unit.
 
-Lemma id_nat_cu_validate : hhead (genv_denote (exprs id_nat_cu)) hnil = id_nat_elim.
-reflexivity.
+Print id_nat_cu.
+
+Eval cbv in (exprs id_nat_cu).
+
+(* Prove that denotation of reflection is same as original *)
+Lemma id_nat_cu_validate :
+  hhead (genv_denote (exprs id_nat_cu)) hnil = id_nat_elim.
+Proof.
+  reflexivity.
 Qed.
 
-Require oeuf.Pretty.
-
-Oeuf Eval lazy Then Write To File "id_nat.oeuf" (Pretty.compilation_unit.print id_nat_cu).
+(* Serialize to a file *)
+Oeuf Eval lazy Then Write To File "id_nat.oeuf"
+     (Pretty.compilation_unit.print id_nat_cu).
