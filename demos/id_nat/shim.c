7c1ce45a5d5a342e809e08845ae788a85fc27514
Eric A Mullen <emullen@cs.washington.edu>
2018-01-04 14:02:12 -0800

    made shim simpler

--- demos/id_nat/shim.c
+++ demos/id_nat/shim.c
@@ -3,39 +3,40 @@
 
 oeuf_function id_nat_elim;
 
-void show_nat(const char* desc, union nat* n_) {
-#ifdef PRINT_LONG
-    printf("oeuf %s: [nat %p] = ", desc, n_);
-    print_nat(n_);
-    printf("\n");
-#else
-    printf("oeuf %s: [nat %p]\n", desc, n_);
-#endif
-}
-
 int main() {
+
+    //get input from user
     int n;
     printf("input? ");
     scanf("%d", &n);
-    if (n < 0) {
+    
+    if (n < 0) { //nats can't be negative
         printf("input must be positive\n");
         return 1;
     }
 
+    //build nat with O and S constructors
     union nat* n_ = make_O();
     for (int i = 0; i < n; ++i) {
         n_ = make_S(n_);
     }
-    show_nat("input", n_);
+    
+    printf("oeuf input: ");
+    print_nat(n_);
 
+    //call ID id_nat_elim defined in Coq
     union nat* m_ = OEUF_CALL(id_nat_elim, n_);
-    show_nat("output", m_);
 
+    printf("oeuf output: ");
+    print_nat(m_);
+
+    //convert result back to from nat
     int m = 0;
     while (m_->tag == TAG_nat_S) {
         ++m;
         m_ = m_->S.n;
     }
+
     printf("output = %d\n", m);
 
     return 0;
