eb900c10d3a6109958fe2d4b1d5e794e32a2c421
Eric A Mullen <emullen@cs.washington.edu>
2018-01-04 14:43:30 -0800

    more demo changes

--- demos/id_nat/Oeuf.v
+++ demos/id_nat/Oeuf.v
@@ -2,4 +2,18 @@ Require Export oeuf.HList oeuf.Utopia oeuf.SourceLifted oeuf.SourceValues oeuf.C
 Require Export OeufPlugin.OeufPlugin.
 Require oeuf.Pretty.
 
+Require Import List.
+Import ListNotations.
 
+Definition cu_denote
+{a b : SourceLifted.type }
+{ttypes : list
+              (SourceLifted.type *
+               list SourceLifted.type *
+               SourceLifted.type)}
+    (exprs : genv ((a,[],b) :: ttypes)) 
+
+:=
+  hhead (genv_denote exprs) hnil.
+
+Arguments nat_rect {_} _ _ _.
