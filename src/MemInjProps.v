08239c6fbc6b8111bf20df57276377fcac096860
Stuart Pernsteiner <spernste@cs.washington.edu>
2018-03-19 16:15:46 -0700

    progress on dflatmajor opaqueop case

--- src/MemInjProps.v
+++ src/MemInjProps.v
@@ -2,6 +2,8 @@ Require Import compcert.common.Values.
 Require Import compcert.common.Memory.
 Require Import compcert.common.Globalenvs.
 
+Require Import ZArith.
+
 Local Open Scope Z_scope.
 
 
@@ -17,4 +19,3 @@ Definition globals_inj_same {F V} (ge : Genv.t F V) (mi : meminj) : Prop :=
     (Genv.find_funct_ptr ge b = Some f \/
     Genv.find_var_info ge b = Some v) ->
     mi b = Some (b,0).
-
