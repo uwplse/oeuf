0e1e68d12b33bb363646ed182106055f418e4221
Eric A Mullen <emullen@cs.washington.edu>
2018-03-29 11:24:03 -0700

    spaces...yay bash

--- scripts/install_coq85.sh
+++ scripts/install_coq85.sh
@@ -4,7 +4,7 @@ set -evuo pipefail
 IFS=$'\n\t'
 
 #if we don't already have a coqc command, download coq and install from source
-if ![-x "$(command -v coqc)"]; then
+if ! [ -x "$(command -v coqc)" ]; then
 
     wget -P $HOME/.cache/coq "https://coq.inria.fr/distrib/V8.5pl3/files/coq-8.5pl3.tar.gz"
     pushd $HOME/.cache/coq
