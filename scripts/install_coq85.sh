841957e12f0ccfab66d2dd531a9965eca8b30912
Eric A Mullen <emullen@cs.washington.edu>
2018-03-29 11:22:03 -0700

    give travis a spin

---
+++ scripts/install_coq85.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -evuo pipefail
+IFS=$'\n\t'
+
+#if we don't already have a coqc command, download coq and install from source
+if ![-x "$(command -v coqc)"]; then
+
+    wget -P $HOME/.cache/coq "https://coq.inria.fr/distrib/V8.5pl3/files/coq-8.5pl3.tar.gz"
+    pushd $HOME/.cache/coq
+    tar -xvf coq-8.5pl3.tar.gz
+    pushd coq-8.5pl3
+    ./configure
+    make
+    
+    popd
+    popd
+fi
