6994de8df087cf2629259bf173e9d42aab36d974
Eric A Mullen <emullen@cs.washington.edu>
2018-03-30 15:52:27 -0700

    idk

--- scripts/install_ocaml.sh
+++ scripts/install_ocaml.sh
@@ -9,7 +9,8 @@ if ! [ -x "$(command -v coqc)" ]; then
 
     opam init -y --comp=4.02.3
     opam update
-    opam install camlp5 menhir
+    opam install camlp5
+    opam install menhir
     eval $(opam config env)
     
 fi
